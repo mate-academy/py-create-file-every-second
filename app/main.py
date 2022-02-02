71ba18f91785f01646e132722770b9181c596d52
Nazar-kob <kobrinnazar05@mail.com>
2022-02-02 15:13:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import datetime
+import time
+
+while True:
+
+    date_now = str(datetime.datetime.now())
+    full_date = f"app-{date_now.split(' ')[1][:8].replace(':', '_')}.log"
+
+    with open(full_date, 'w') as file:
+        file.write(date_now)
+
+    print(date_now + ' ' + full_date)
+    time.sleep(1)
