c80980e48b9e97fe3403dbefed9a73c4ccc569c9
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-02 14:20:10 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -5,9 +5,16 @@ import time
 def app():
     while True:
         file_name = datetime.now()
-        with open(f'app-{file_name.hour}_{file_name.minute}_{file_name.second}.log', 'w') as file:
-            file.writelines(str(file_name) + f' app-{file_name.hour}_{file_name.minute}_{file_name.second}.log')
-            print(file_name, f'app-{file_name.hour}_{file_name.minute}_{file_name.second}.log')
+        with open(
+                f'app-{file_name.hour}_{file_name.minute}'
+                f'_{file_name.second}.log', 'w'
+        ) as file:
+            file.writelines(
+                str(file_name) + f' app-{file_name.hour}'
+                                 f'_{file_name.minute}_{file_name.second}.log'
+            )
+            print(file_name, f'app-{file_name.hour}'
+                             f'_{file_name.minute}_{file_name.second}.log')
         time.sleep(1)
 
 
