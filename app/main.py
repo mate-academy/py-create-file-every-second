d1cad829441a4b9611fa549cf746a11e6c097665
Alex Gergel <alexgergel@gmail.com>
2022-02-02 17:01:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import time
 
 while True:
     timestamp = datetime.datetime.now().strftime("%H_%M_%S")
+
     with open("app-" + timestamp + ".log", "w") as f:
         f.write(str(datetime.datetime.now()))
         print(str(datetime.datetime.now()), "app-" + timestamp + ".log")
