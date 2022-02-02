d5f6c9b33045d6a9077a693483d1784da4a02f27
Kalyta Valerii <deptovka@gmail.com>
2022-02-02 16:51:21 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+import datetime
+import time
+
+
+def create_file_every_second():
+    now = datetime.datetime.now
+    while True:
+        file_name = 'app-' + now().strftime("%H_%M_%S") + '.log'
+        content = now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        with open(file_name, "w") as f:
+            f.write(content)
+            print(content, file_name)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    create_file_every_second()
