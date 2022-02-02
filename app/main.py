6b16ccfde92603eaef3ffd6eaf7ae73fd0c71fba
Alex Gergel <alexgergel@gmail.com>
2022-02-02 17:00:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+import datetime
+import time
+
+while True:
+    timestamp = datetime.datetime.now().strftime("%H_%M_%S")
+    with open("app-" + timestamp + ".log", "w") as f:
+        f.write(str(datetime.datetime.now()))
+        print(str(datetime.datetime.now()), "app-" + timestamp + ".log")
+        time.sleep(1)
