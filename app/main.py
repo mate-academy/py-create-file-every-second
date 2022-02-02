8dad197af909148edb8bc772206cbecd86561b77
Rybak Dmytro <codingclaud@gmail.com>
2022-02-02 14:43:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import datetime
+import time
+
+
+def files_creator():
+    while True:
+        time_now = datetime.datetime.now()
+        time_now = "app-" +\
+                   str(time_now.hour) +\
+                   "_" + str(time_now.minute) +\
+                   "_" + str(time_now.second) +\
+                   ".log"
+
+        with open(time_now, "w") as f:
+            f.write(str(datetime.datetime.now()))
+            print(datetime.datetime.now(), time_now)
+        time.sleep(1)
+
+
+files_creator()
