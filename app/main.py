93427d2e17dbc45b4e8be62d78676342f41d506d
TJgkOsVP <sasha06052001@gmail.com>
2022-02-02 16:40:22 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+from datetime import datetime
+from time import sleep
+
+
+def files_generator():
+    while True:
+        t = datetime.now()
+        file_name = f"app-{t.hour}_{t.minute}_{t.second}.log"
+        print(str(t), file_name)
+        sleep(1)
+        with open(file_name, "a+") as f:
+            f.write(str(t))
+
+
+if __name__ == "__main__":
+    files_generator()
