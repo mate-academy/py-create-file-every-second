d7ee9d2099a362567c031e44927b8ed485f499e3
alix makarova <alix.makarova@gmail.com>
2022-02-02 14:33:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import datetime
+import time
+
+
+def create_a_file_every_second():
+    while True:
+        now = str(datetime.datetime.now())
+        file_name = f"app-{now[11:13]}_{now[15:17]}_{now[18:20]}.log"
+
+        with open(file_name, "w") as f:
+            f.write(f"{datetime.datetime.now()}" + "\n")
+
+        print(f"The file {file_name} created successfully")
+        time.sleep(1)
