9acc47cf2de06593e9165ecbae996685eb69dedf
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-02 17:18:22 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+from datetime import datetime
+import time
+
+
+while True:
+    now = datetime.now()
+    file_name = f"app-{now.strftime('%H')}_{now.strftime('%M')}_{now.strftime('%S')}.log"
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(str(now))
+    print(f"{now} {file_name}")
+    time.sleep(1)
