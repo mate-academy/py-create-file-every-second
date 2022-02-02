862b492781c3d5b2b995d0365dea7be92a394b12
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-02 17:56:33 +0200

    small fix

--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ from time import sleep
 
 def file_creating():
     while True:
-        file_name = f"app-{datetime.now().strftime('%H_%M_%-S')}.log"
+        file_name = f"app-{datetime.today().strftime('%H_%M_%-S')}.log"
         timestamp = datetime.today().strftime("%Y-%m-%d %H:%M:%S.%f")
         with open(file_name, "w") as file:
             file.writelines(timestamp)
             print(timestamp, file_name)
-
         sleep(1)
 
 
