e4587688abb9664b5b1727196d435348f07a21e5
Ihor Olifirenko <igoroliphirenko@gmail.com>
2022-02-02 16:59:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from time import sleep
+from datetime import datetime
+
+
+def create_log_every_second():
+
+    while True:
+        current_time = datetime.now()
+        file_name = f"app-{current_time.hour}" \
+                    f"_{current_time.minute}" \
+                    f"_{current_time.second}.log"
+        with open(file_name, "w") as f:
+            f.write(str(current_time))
+            print(current_time, file_name)
+        sleep(1)
+
+
+if __name__ == '__main__':
+    create_log_every_second()
