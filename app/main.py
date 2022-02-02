9a9393033a98bfd4a10cc84171c9e70c0dad90b1
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-02 15:46:16 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import datetime
+import time
+
+
+def logs():
+    while True:
+        currenttime = datetime.datetime.now()
+        hours = currenttime.hour
+        minutes = currenttime.minute
+        seconds = currenttime.second
+        with open(f"app-{hours}_{minutes}_{seconds}.log", "a") as f:
+            f.writelines(f"{currenttime}")
+        with open(f"app-{hours}_{minutes}_{seconds}.log", "r") as f:
+            print(f"{f.read()} app-{hours}_{minutes}_{seconds}.log")
+        time.sleep(1)
