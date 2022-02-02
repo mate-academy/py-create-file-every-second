96e339adaf293662488ee074fb5961a833d5b9a5
Nazar-kob <kobrinnazar05@mail.com>
2022-02-02 19:32:03 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 import datetime
 import time
 
-while True:
 
-    date_now = datetime.datetime.now()
-    full_date = f"app-{date_now.hour}_{date_now.minute}_{date_now.second}.log"
+def every_second_file():
+    while True:
+        date_now = datetime.datetime.now()
+        full_date = f"app-{date_now.hour}" \
+                    f"_{date_now.minute}" \
+                    f"_{date_now.second}.log"
 
-    with open(full_date, 'w') as file:
-        file.write(str(date_now))
+        with open(full_date, 'w') as file:
+            file.write(str(date_now))
 
-    print(str(date_now) + ' ' + full_date)
-    time.sleep(1)
+        print(str(date_now) + ' ' + full_date)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    every_second_file()
