1c3ac54ff469a65cc9a1bf7e805025073b23b899
Nazar-kob <kobrinnazar05@mail.com>
2022-02-02 19:25:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import time
 
 while True:
 
-    date_now = str(datetime.datetime.now())
-    full_date = f"app-{date_now.split(' ')[1][:8].replace(':', '_')}.log"
+    date_now = datetime.datetime.now()
+    full_date = f"app-{date_now.hour}_{date_now.minute}_{date_now.second}.log"
 
     with open(full_date, 'w') as file:
-        file.write(date_now)
+        file.write(str(date_now))
 
-    print(date_now + ' ' + full_date)
+    print(str(date_now) + ' ' + full_date)
     time.sleep(1)
