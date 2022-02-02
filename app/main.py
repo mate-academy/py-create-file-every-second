d8febe35e7c6ea1f5b510546fd170d1cf8ba79c2
Anita Posledova <anitaposliedova@gmail.com>
2022-02-02 22:28:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import datetime
+import time
+
+
+def files_creator():
+    while True:
+        accurate_time = datetime.datetime.now()
+        accurate_time = (f"app-{str(accurate_time.hour)}"
+                         f"_{str(accurate_time.minute)}"
+                         f"_{str(accurate_time.second)}.log")
+
+        with open(accurate_time, "w") as f:
+            f.write(str(datetime.datetime.now()))
+            print(datetime.datetime.now(), accurate_time)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    files_creator()
