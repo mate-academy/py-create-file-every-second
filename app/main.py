5c7e316c783771c35611f6bb9863ef2c3bbe69d0
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-02 15:25:52 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from datetime import datetime
+from time import sleep
+
+
+def file_creating():
+    while True:
+        file_name = f"app-{datetime.now().strftime('%H_%M_%-S')}.log"
+        timestamp = datetime.today().strftime("%Y-%m-%d %H:%M:%S.%f")
+        with open(file_name, "w") as file:
+            file.writelines(timestamp)
+            print(timestamp, file_name)
+
+        sleep(1)
+
+
+if __name__ == "__main__":
+    file_creating()
