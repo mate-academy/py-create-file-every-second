8c60a126693ff71b0140cc175f856a900a119e4c
Zikkolion <nmadaraosnova835@gmail.com>
2022-02-03 17:20:19 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import datetime
+import time
+
+
+def create_a_file_every_sevond():
+    while True:
+        moment = datetime.datetime.now()
+        name = f'app-{moment.hour}_{moment.minute}_{moment.second}.log'
+        with open(f'{name}', 'w') as f:
+            f.write(str(moment))
+        print(str(moment), name)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    create_a_file_every_sevond()
