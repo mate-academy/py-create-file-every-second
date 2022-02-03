fd2f35cac264d590e9751a0e3f2d039d88179cbe
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-03 15:35:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from datetime import datetime
 
 def create_file_every_second():
     while True:
-        file_name = f"app-{datetime.now().hour}_{datetime.now().minute}_" \
-                    f"{datetime.now().second}.log"
-        file_content = str(datetime.now())
+        cur_time = datetime.now()
+        file_name = f"app-{cur_time.hour}_{cur_time.minute}_" \
+                    f"{cur_time.second}.log"
+        file_content = str(cur_time)
         with open(file_name, "w") as f:
             f.write(file_content)
             print(file_content, file_name)
