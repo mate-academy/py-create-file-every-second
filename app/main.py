883008647d22ef42b3c1d88304424771f2e15041
Rybak Dmytro <codingclaud@gmail.com>
2022-02-03 16:28:52 +0200

    'Fix_logic'

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ import time
 def files_creator():
     while True:
         time_now = datetime.datetime.now()
-        time_now = "app-" +\
-                   str(time_now.hour) +\
-                   "_" + str(time_now.minute) +\
-                   "_" + str(time_now.second) +\
-                   ".log"
+        new_file_name = "app-" +\
+            str(time_now.hour) +\
+            "_" + str(time_now.minute) +\
+            "_" + str(time_now.second) +\
+            ".log"
 
-        with open(time_now, "w") as f:
-            f.write(str(datetime.datetime.now()))
-            print(datetime.datetime.now(), time_now)
+        with open(new_file_name, "w") as f:
+            f.write(str(time_now))
+            print(time_now, new_file_name)
         time.sleep(1)
 
 
