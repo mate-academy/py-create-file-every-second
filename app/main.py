172ffde417689a1b3455eb4ebb9a71edd688b945
Dmitry Ivanov <karward94@gmail.com>
2022-02-03 09:58:11 +0200

    Fix print to console.

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def loging():
     while True:
         with open(datetime.now().strftime("app-%H_%M_%S.log"), "w+") as file:
             file.write(str(datetime.now()))
+        print(str(datetime.now()), datetime.now().strftime("app-%H_%M_%S.log"))
         sleep(1)
 
 
