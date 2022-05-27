fe2d0543eb6b124be94984429cace8b2b133b210
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-27 21:48:31 +0300

    one more fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import time
 
 
 while True:
+
     time.sleep(1)
+
     current_time = datetime.now()
     time_for_name = current_time.time().isoformat(timespec="seconds")
     name_of_file = f'app-{time_for_name.replace(":", "_")}.log'
