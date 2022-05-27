6a03375b50d98113d614a73ec7376741244ca847
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-27 11:42:32 +0300

    refactor code)

--- app/main.py
+++ app/main.py
@@ -4,19 +4,19 @@ import time
 
 
 while True:
+
     file_content = datetime.now()
+
     hour = file_content.hour
     minute = file_content.minute
     second = file_content.second
-    file_name = f"{os.path.basename(os.getcwd())}" \
-                f"-{hour}" \
-                f"_{minute}" \
-                f"_{second}.log"
+
+    last_part_file_name = f"-{hour}_{minute}_{second}.log"
+    file_name = f"{os.path.basename(os.getcwd())}{last_part_file_name}"
+
     with open(file_name, "w", encoding="utf8") as log_file:
         log_file.write(f"{file_content}\n")
         print(f"{file_content}"
               f" {os.path.basename(os.getcwd())}"
-              f"-{hour}"
-              f"_{minute}"
-              f"_{second}.log")
+              f"{last_part_file_name}")
         time.sleep(1)
