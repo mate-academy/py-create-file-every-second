c8f55847673d1807aec2cdcba8077fe248c758e5
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-27 21:46:22 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -5,8 +5,10 @@ import time
 while True:
     time.sleep(1)
     current_time = datetime.now()
-    name_of_file = f'app-{current_time.time().isoformat(timespec="seconds").replace(":", "_")}.log'
+    time_for_name = current_time.time().isoformat(timespec="seconds")
+    name_of_file = f'app-{time_for_name.replace(":", "_")}.log'
     current_time_iso = current_time.isoformat(sep=" ")
+
     with open(name_of_file, "w") as f:
         f.write(current_time_iso)
         print(current_time_iso, name_of_file)
