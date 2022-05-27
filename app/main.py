50deea4908659b60121893c5fd04019a811f501a
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-27 10:58:00 +0300

    Fix remark

--- app/main.py
+++ app/main.py
@@ -5,15 +5,18 @@ import time
 
 while True:
     file_content = datetime.now()
+    hour = file_content.hour
+    minute = file_content.minute
+    second = file_content.second
     file_name = f"{os.path.basename(os.getcwd())}" \
-                f"-{file_content.hour}" \
-                f"_{file_content.minute}" \
-                f"_{file_content.second}.log"
+                f"-{hour}" \
+                f"_{minute}" \
+                f"_{second}.log"
     with open(file_name, "w", encoding="utf8") as log_file:
         log_file.write(f"{file_content}\n")
         print(f"{file_content}"
               f" {os.path.basename(os.getcwd())}"
-              f"-{file_content.hour}"
-              f"_{file_content.minute}"
-              f"_{file_content.second}.log")
+              f"-{hour}"
+              f"_{minute}"
+              f"_{second}.log")
         time.sleep(1)
