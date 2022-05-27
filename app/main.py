5726856f001b8c6a4a3e0682d1d5bc4168eec4cb
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-27 21:34:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+from datetime import datetime
+import time
+
+
+while True:
+    time.sleep(1)
+    current_time = datetime.now()
+    name_of_file = f'app-{current_time.time().isoformat(timespec="seconds").replace(":", "_")}.log'
+    current_time_iso = current_time.isoformat(sep=" ")
+    with open(name_of_file, "w") as f:
+        f.write(current_time_iso)
+        print(current_time_iso, name_of_file)
