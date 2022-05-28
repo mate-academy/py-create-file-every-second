c6eeb544170af9445f02b33e6d80d036431dc161
Taras Nester <tarasnester414@gmail.com>
2022-05-28 15:34:06 +0300

    base version

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+from datetime import datetime
+from time import sleep
+
+
+while True:
+    current_time = datetime.now()
+
+    file_name = current_time.strftime("app-%H_%M_%S.log")
+    with open(file_name, "w") as log_file:
+        log_file.write(str(current_time))
+
+    print(current_time, file_name)
+    sleep(1)
