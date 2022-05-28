88e1caa418b56c8befa0fe92d5fa252bb898d345
Flashmobber <ufinaid@gmail.com>
2022-05-28 18:01:49 +0300

    All done.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from datetime import datetime
+
+start = datetime.now().second
+
+while True:
+    now = datetime.now()
+    while start == now.second:
+        now = datetime.now()
+
+    start = now.second
+
+    try:
+        file = open(f"app-{now.hour}_{now.minute}_{now.second}.log", "w")
+        file.write(str(now))
+    except IOError as e:
+        print(e)
+    else:
+        print(now, file.name)
+        file.close()
