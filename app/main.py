4dd9341c0e52db3b2079c20963496907cb5ba4be
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-29 20:59:10 +0300

    fixed the code

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from time import sleep
 
 def file_creator():
     while True:
-        with open(f"app-{datetime.now().hour}"
-                  f"_{datetime.now().minute}"
-                  f"_{datetime.now().second}.log", "w") as file:
-
-            file.write(str(datetime.now()))
+        time_now = datetime.now()
+        file_name = f"app-{time_now.hour}"\
+                    f"_{time_now.minute}"\
+                    f"_{time_now.second}.log"
+        with open(file_name, "w") as file:
+            file.write(str(time_now))
+        print(str(time_now) + ' ' + file_name)
         sleep(1)
 
 
