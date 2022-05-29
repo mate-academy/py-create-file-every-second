aeab80da251e0d6c8242783aa7fafc1bbc06b483
Dima Svirsa <august4smile@gmail.com>
2022-05-29 11:53:41 +0300

    Solution without errors

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from time import sleep
 
 while True:
     date_now = datetime.now()
-    time_info = f"{date_now().hour}_" \
-                f"{date_now().minute}_" \
-                f"{date_now().second}"
+    time_info = f"{datetime.now().hour}_" \
+                f"{datetime.now().minute}_" \
+                f"{datetime.now().second}"
     with open(f"{time_info}.log", "w") as log_file:
         log_file.write(f"{date_now}")
         print(f"file with name {time_info}.log "
