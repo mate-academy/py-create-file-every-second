f3eba819d5be84942a13944d17c72260ecec2b85
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-05-29 20:40:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from time import sleep
+from datetime import datetime
+
+
+while True:
+    sleep(1)
+    date = datetime.now()
+    time_info = f"{datetime.now().hour}_" \
+                f"{datetime.now().minute}_" \
+                f"{datetime.now().second}"
+    with open(f"{time_info}.log", "w") as log_file:
+        log_file.write(f"{date}")
+        print(f"file with name {time_info}.log "
+              f"was created {date}")
