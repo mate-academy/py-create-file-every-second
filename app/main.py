52a0b24a9e0ad775f3afd479a9a991f449b2340d
bohdan <bodyan1108@gmail.com>
2022-05-30 17:32:57 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+from datetime import datetime
+from time import sleep
+
+
+def w_file_every_second():
+    for _ in range(3):
+        now = datetime.now()
+        filename = now.strftime("app-%H_%M_%S.log")
+        with open(filename, "w") as f:
+            f.write(f"{now}")
+            sleep(1)
