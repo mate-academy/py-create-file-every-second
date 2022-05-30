428bf599b3aadabfa5b973d08a66fe588d106c8a
Hectorovich <andrew.gutnik10@gmail.com>
2022-05-30 18:26:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import datetime
+import time
+
+
+while True:
+    now = datetime.datetime.now()
+    file_name = " app-" + \
+                f"{now.strftime('%H')}_" \
+                f"{now.strftime('%M')}_" \
+                f"{now.strftime('%S')}" + \
+                ".log"
+    with open(file_name, "w") as f:
+        content = (
+            f"{now.strftime('%Y')}-"
+            f"{now.strftime('%m')}-"
+            f"{now.strftime('%d')} "
+            f"{now.strftime('%X')}."
+            f"{now.strftime('%f')}"
+        )
+        f.write(content)
+        print(content + file_name)
+        time.sleep(1)
