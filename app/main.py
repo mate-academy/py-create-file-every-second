f410718ef5b9b8c2903caeb66acef6966a1e13e0
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-30 19:47:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+from datetime import datetime
+import time
+
+
+def main():
+    while True:
+        t = datetime.now()
+        with open(f'app-{t.hour}_{t.minute}_{t.second}.log', 'w') as f:
+            f.write(str(t))
+            print(f'File {f.name} created at {str(t)}')
+            time.sleep(1)
