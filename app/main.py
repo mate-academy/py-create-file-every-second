0d115902d59046d239fb5237d2fda91cbe2fd3cd
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-30 20:00:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from datetime import datetime
-import time
+from time import sleep
 
 
 def main():
     while True:
-        time.sleep(1)
+        sleep(1)
         t = datetime.now()
         with open(f'app-{t.hour}_{t.minute}_{t.second}.log', 'w') as f:
             f.write(str(t))
-            print(f'File {f.name} created at {str(t)}')
+            print(f.name, str(t))
+
+main()
