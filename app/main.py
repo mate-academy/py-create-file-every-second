34b18b01dfb6da30aacba7ab081595ce07942ef4
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-30 19:54:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ import time
 
 def main():
     while True:
+        time.sleep(1)
         t = datetime.now()
         with open(f'app-{t.hour}_{t.minute}_{t.second}.log', 'w') as f:
             f.write(str(t))
             print(f'File {f.name} created at {str(t)}')
-            time.sleep(1)
