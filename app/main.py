ba5b62f9b76796a40aa59c8b58d0bf056e108961
Dorius <kornelydorius@gmail.com>
2022-05-30 13:17:28 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,9 +5,12 @@ from datetime import datetime
 def main():
     while True:
         sleep(1)
-        with open(f"app-{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}.log", "w") as f:
+        ct = datetime.now()
+        name = f"app-{ct.hour}_{ct.minute}_{ct.second}.log"
+        with open(name, "w") as f:
             f.write(str(datetime.now()))
         print(datetime.now(), f.name)
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     main()
