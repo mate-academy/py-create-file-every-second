c9cbbe3d06b97349aa5feb78e2bcfb3539bb4708
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-30 20:32:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,13 @@ import time
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        t = datetime.datetime.now()
+        name = t.strftime('app-%H_%M_%S.log')
+        with open(name, 'w') as f:
+            f.write(str(t))
+            print(str(t), f.name)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
