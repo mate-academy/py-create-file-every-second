29dae2abf395f9ff561a72e36149efe1e0d01a1b
Dorius <kornelydorius@gmail.com>
2022-05-30 13:13:48 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+from time import sleep
+from datetime import datetime
+
+
+def main():
+    while True:
+        sleep(1)
+        with open(f"app-{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}.log", "w") as f:
+            f.write(str(datetime.now()))
+        print(datetime.now(), f.name)
+
+if __name__ =='__main__':
+    main()
