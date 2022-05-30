424968dfe74a8f9c1963fc7b6998e399c5a7c9c2
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-30 11:28:19 +0300

    ADD function

--- app/main.py
+++ app/main.py
@@ -3,20 +3,21 @@ from datetime import datetime
 import time
 
 
-while True:
+def create_file_every_second():
+    while True:
 
-    file_content = datetime.now()
+        file_content = datetime.now()
 
-    hour = file_content.hour
-    minute = file_content.minute
-    second = file_content.second
+        hour = file_content.hour
+        minute = file_content.minute
+        second = file_content.second
 
-    last_part_file_name = f"-{hour}_{minute}_{second}.log"
-    file_name = f"{os.path.basename(os.getcwd())}{last_part_file_name}"
+        last_part_file_name = f"-{hour}_{minute}_{second}.log"
+        file_name = f"{os.path.basename(os.getcwd())}{last_part_file_name}"
 
-    with open(file_name, "w", encoding="utf8") as log_file:
-        log_file.write(f"{file_content}\n")
-        print(f"{file_content}"
-              f" {os.path.basename(os.getcwd())}"
-              f"{last_part_file_name}")
-        time.sleep(1)
+        with open(file_name, "w", encoding="utf8") as log_file:
+            log_file.write(f"{file_content}\n")
+            print(f"{file_content}"
+                  f" {os.path.basename(os.getcwd())}"
+                  f"{last_part_file_name}")
+            time.sleep(1)
