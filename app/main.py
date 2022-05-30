e7c09b8708ffd018258654fcb48c31ef0f2086dc
Serhii Leonenko <leonenko3149@gmail.com>
2022-05-30 21:14:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,8 +3,16 @@ import time
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        current_time = datetime.datetime.now()
+        time_for_name = current_time.time().isoformat(timespec="seconds")
+        name_of_file = f'app-{time_for_name.replace(":", "_")}.log'
+        current_time_iso = current_time.isoformat(sep=" ")
+
+        with open(name_of_file, "w") as f:
+            f.write(current_time_iso)
+            print(current_time_iso, name_of_file)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
