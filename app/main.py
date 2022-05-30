f9a7b7837e79ff16e1b3e087fb133f359d0e205b
Roma Tsybulia <Romalonstejn@gmail.com>
2022-05-30 17:56:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+from datetime import datetime
+import time
+
+
+while True:
+    file_name = "app-" + datetime.now().strftime("%H_%M_%S") + ".log"
+    content = str(datetime.now())
+    with open(file_name, "x") as file:
+        file.write(content)
+    time.sleep(1)
