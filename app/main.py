8e4b71d709eea7be1ecc7bcfba97f4c1eb15aba4
Hectorovich <andrew.gutnik10@gmail.com>
2022-05-30 19:17:55 +0300

    Finally

--- app/main.py
+++ app/main.py
@@ -2,22 +2,22 @@ import datetime
 import time
 
 
-while True:
-    now = datetime.datetime.now()
-    file_name = " app-" + \
-                f"{now.strftime('%H')}_" \
-                f"{now.strftime('%M')}_" \
-                f"{now.strftime('%S')}" + \
-                ".log"
-    with open(file_name, "w") as f:
-        content = (
-            f"{now.strftime('%Y')}-"
-            f"{now.strftime('%m')}-"
-            f"{now.strftime('%d')} "
-            f"{now.strftime('%X')}."
-            f"{now.strftime('%f')}"
-        )
+def main():
+    while True:
+        now = datetime.datetime.now()
+        file_name = "app-"\
+                    f"{now.strftime('%H')}_" \
+                    f"{now.strftime('%M')}_" \
+                    f"{now.strftime('%S')}" + \
+                    ".log"
+        with open(file_name, "w") as f:
+            content = (
+                f"{now.strftime('%Y')}-"
+                f"{now.strftime('%m')}-"
+                f"{now.strftime('%d')} "
+                f"{now.strftime('%X')}"
+            )
 
-        f.write(content)
-        print(content + file_name)
-        time.sleep(1)
+            f.write(content)
+            print(content + " " + file_name)
+            time.sleep(1)
