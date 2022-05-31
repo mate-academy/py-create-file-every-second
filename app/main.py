d639ef9c81581590861c1526f6c294e8adc11765
bohdan <bodyan1108@gmail.com>
2022-05-31 14:06:21 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from time import sleep
 
 
 def w_file_every_second():
-    for _ in range(3):
+    while True:
         now = datetime.now()
         filename = now.strftime("app-%H_%M_%S.log")
         with open(filename, "w") as f:
             f.write(f"{now}")
             sleep(1)
+
+        print(now, filename)
