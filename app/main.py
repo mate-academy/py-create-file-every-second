234fa43a581c6be9a17ef01c6436abd917f78054
ILIAS <nossudak@gmail.com>
2022-05-31 13:21:08 +0300

    Fixed formating

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import time
 
 def main():
     while True:
-        t = datetime.now().time()
         date_info = datetime.now()
-        file_name = f"app-{t.hour}_{t.minute}_{t.second}.log"
+        file_name = f"app-{date_info.hour}_{date_info.minute}" \
+                    f"_{date_info.second:02d}.log"
         with open(file_name, "w") as file:
             file.write(str(date_info))
         print(f"{date_info} {file_name}")
