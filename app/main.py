5a7083e4d725eceeac284332fb597cd233bb72d1
Yurnerroo <yura.zdanevych@gmail.com>
2022-05-31 16:27:23 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from time import sleep
 def main():
     while True:
         time = datetime.now()
-        with open(f"app-{time.hour}_{time.minute}_{time.second:02d}.log", "w") as f:
+        with open(f"app-{time.hour}_{time.minute}_{time.second:02d}"
+                  f".log", "w") as f:
             f.write(str(time))
         sleep(1)
         print(f"{time} app-{time.hour}_{time.minute}_{time.second:02d}.log")
