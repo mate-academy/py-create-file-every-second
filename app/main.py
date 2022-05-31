e615986fdaa85e0aa22dd1aba0e5183373250f6c
Dima Svirsa <august4smile@gmail.com>
2022-05-31 12:11:31 +0300

    ?????

--- app/main.py
+++ app/main.py
@@ -8,11 +8,13 @@ def main():
         time_info = f"{datetime.now().hour}_" \
                     f"{datetime.now().minute}_" \
                     f"{datetime.now().second}"
+
         with open(f"app-{time_info}.log", "w") as log_file:
             log_file.write(f"{date_now}")
-            print(f"app-{time_info}.log")
-            print(f"{date_now}")
-            sleep(1)
+
+        print(f"app-{time_info}.log")
+        print(f"{date_now}")
+        sleep(1)
 
 
 if __name__ == "__main__":
