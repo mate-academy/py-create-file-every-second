5ea72cdb58cddba9d703e9b6ba9c8d06806b32f5
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-31 11:43:48 +0300

    'fix'

--- app/main.py
+++ app/main.py
@@ -4,15 +4,17 @@ from time import sleep
 
 def main():
     while True:
-        sleep(1)
         # current time (c_t)
         c_t = datetime.datetime.now()
-        file = f"app-{c_t.hour}_{c_t.minute}_{c_t.second}.log"
+        file = f"app-{c_t.strftime('%H')}_" \
+               f"{c_t.strftime('%M')}_" \
+               f"{c_t.strftime('%S')}.log"
 
         with open(file, "w") as f:
             f.write(str(c_t))
 
         print(c_t, file)
+        sleep(1)
 
 
 if __name__ == "__main__":
