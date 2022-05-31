fc2752d23ef3f2e981041bfd5369713a57635dd5
Magus6784 <100879423+Magus6784@users.noreply.github.com>
2022-05-31 10:07:01 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from time import sleep
 from datetime import datetime
 
+def main():
+  while True:
+      sleep(1)
+      date = datetime.now()
+      time_info = f"{datetime.now().hour}_" \
+                  f"{datetime.now().minute}_" \
+                  f"{datetime.now().second}"
+      with open(f"{time_info}.log", "w") as log_file:
+          log_file.write(f"{date}")
+          print(f"file with name {time_info}.log "
+                f"was created {date}")
 
-while True:
-    sleep(1)
-    date = datetime.now()
-    time_info = f"{datetime.now().hour}_" \
-                f"{datetime.now().minute}_" \
-                f"{datetime.now().second}"
-    with open(f"{time_info}.log", "w") as log_file:
-        log_file.write(f"{date}")
-        print(f"file with name {time_info}.log "
-              f"was created {date}")
+        
+if __name__ == "__main__":
+  main()   
