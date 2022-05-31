fc09fb9a7af33e755081a1cc7747a5c87f3e219c
Dima Svirsa <august4smile@gmail.com>
2022-05-31 12:06:17 +0300

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ def main():
                     f"{datetime.now().second}"
         with open(f"app-{time_info}.log", "w") as log_file:
             log_file.write(f"{date_now}")
-            print(f"file with name app-{time_info}.log "
-                  f"was created {date_now}")
+            print(f"app-{time_info}.log")
+            print(f"{date_now}")
             sleep(1)
 
 
