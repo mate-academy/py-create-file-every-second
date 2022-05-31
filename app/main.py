3c9e42e5a2e3a8886e576a72f173a4dcee7ff8b6
Dima Svirsa <august4smile@gmail.com>
2022-05-31 13:03:16 +0300

    'final_Solution'

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
-from datetime import datetime
-from time import sleep
+import datetime
+import time
 
 
 def main():
     while True:
-        date_now = datetime.now()
-        time_info = f"{datetime.now().hour}_" \
-                    f"{datetime.now().minute}_" \
-                    f"{datetime.now().second}"
+        date_now = datetime.datetime.now()
+        time_info = f"{datetime.datetime.now().hour}_" \
+                    f"{datetime.datetime.now().minute}_" \
+                    f"{datetime.datetime.now().second}"
 
         with open(f"app-{time_info}.log", "w") as log_file:
             log_file.write(f"{date_now}")
 
         print(f"app-{time_info}.log")
         print(f"{date_now}")
-        sleep(1)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
