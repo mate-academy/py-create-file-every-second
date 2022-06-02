8513957ff5b42509d35aa363e7bb5b3ebfce27e1
Mariia Movchan <mariia.movchan.s@gmail.com>
2022-06-02 08:45:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
 def main():
     while True:
         file_name = datetime.now().strftime("app-%H_%M_%S.log")
-        file_content = str(datetime.now())
+        content = str(datetime.now())
 
         with open(file_name, "w") as f:
-            f.write(file_content)
+            f.write(content)
 
         with open(file_name, "r") as f:
             print(f"{f.read()} {file_name}")
