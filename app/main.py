67c26403d20b7e116c2cd17a4f41a68bac67f640
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-07 17:18:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        t = str(datetime.now())
+        name_for_task = "app-" + t[11:13] + "_" \
+                        + t[14:16] + "_" + t[17:19] + ".log"
+        with open(name_for_task, 'w') as f:
+            f.write(t)
+            print(t + " " + name_for_task)
+            sleep(1)
 
 
 if __name__ == "__main__":
