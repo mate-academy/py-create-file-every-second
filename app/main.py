3a3d0f2fb1cb44732f5674bee589118f3cf3f7fd
Inna Kushnir <innakushnir63@gmail.com>
2022-07-30 22:26:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+import time
 
 
 def main():
-    # write your code here
-    pass
+
+    while 1:
+        f_ = str(datetime.now())
+        with open("app-" + f_[11:13] + "_" + f_[14:16] +
+                  "_" + f_[17:19] + ".log", "w+") as f:
+            f.write(str(datetime.now()))
+            print((str(datetime.now()) + " app-" +
+                   f_[11:13] + "_" + f_[14:16] + "_" + f_[17:19] + ".log"))
+            time.sleep(1)
 
 
 if __name__ == "__main__":
