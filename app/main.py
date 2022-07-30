4967c7d9a92f7a71d4c3c3992ad9e0eafa86cec2
Inna Kushnir <innakushnir63@gmail.com>
2022-07-30 22:48:20 +0300

    flake8

--- app/main.py
+++ app/main.py
@@ -6,11 +6,14 @@ def main():
 
     while 1:
         f_ = str(datetime.now())
-        with open("app-" + f_[11:13] + "_" + f_[14:16] +
-                  "_" + f_[17:19] + ".log", "w+") as f:
+        with open(
+                "app-" + f_[11:13] + "_" + f_
+                [14:16] + "_" + f_[17:19] + ".log", "w+") as f:
             f.write(str(datetime.now()))
-            print((str(datetime.now()) + " app-" +
-                   f_[11:13] + "_" + f_[14:16] + "_" + f_[17:19] + ".log"))
+            print((str(
+                datetime.now()) + " app-"
+                                  "" + f_[11:13] + "_" + f_
+                [14:16] + "_" + f_[17:19] + ".log"))
             time.sleep(1)
 
 
