f34210e030ee63dd68186224bd3c361a0797e46f
Dima Povkh <dimapovkh@gmail.com>
2022-08-03 15:10:53 +0200

    file_every_second_v1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        now = datetime.now()
+        with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "w") as f:
+            f.write(str(datetime.now()))
+            print(str(now) + f" app-{now.hour}_{now.minute}_{now.second}.log")
+            time.sleep(1)
 
 
 if __name__ == "__main__":
