2c4cef926fc3d16f825756bea3267aab6e91eb06
yaroslav-demchenko <dastex1411@gmail.com>
2022-08-07 18:58:33 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        content = str(datetime.now())
+        q = content.split(" ")
+        x = q[1].split(".")
+        hms = x[0].split(":")
+        file_name = f"app-{hms[0]}_{hms[1]}_{hms[2]}.log"
+        with open(file_name, "w") as f:
+            f.write(content)
+        with open(file_name, "r") as f:
+            print(f"{f.readline()} {f.name}")
+        sleep(1)
 
 
 if __name__ == "__main__":
