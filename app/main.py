206947d5baaa662ae4d107dd78674f7efe7fb3cb
Paul Starodub <amorallex@gmail.com>
2022-08-31 22:48:42 +0300

    Solo

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        info_date = str(datetime.now())
+        with open(f"app-{info_date[11:13]}_"
+                  f"{info_date[14:16]}_{info_date[17:19]}.log", "w") as f:
+            f.write(info_date)
+        print(info_date, end=" ")
+        print(f"app-{info_date[11:13]}_{info_date[14:16]}"
+              f"_{info_date[17:19]}.log")
+        sleep(1)
 
 
 if __name__ == "__main__":
