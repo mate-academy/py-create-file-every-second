e92510d795a13a266aaace02cbdc9224c809b5d1
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-08 21:43:42 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ class ContexManager:
         self.file = None
 
     def __enter__(self):
-        self.file = open(f"app-{self.hour}_{self.minute}_{self.second}.log", self.mode)
+        self.file = open(f"app-{self.hour}_{self.minute}_{self.second}.log",
+                         self.mode)
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
