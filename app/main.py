ffa18383c30b8b6427a6a3a054834e6494433735
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-16 17:53:16 +0300

    py_create_file_every_second_Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        date_time = datetime.now()
+        filename = \
+            f"app-{date_time.hour}_{date_time.minute}_{date_time.second}.log"
+        with open(filename, "w") as file:
+            file.write(str(date_time))
+            print(f"{date_time} {filename}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
