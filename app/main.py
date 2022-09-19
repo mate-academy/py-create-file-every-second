cf69109c32e0c5f4420673c9dc58869d0a02d961
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-19 18:21:41 +0300

    Solution for creation every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        date = datetime.now()
+        name_of_file = f"app-{date.hour}_{date.minute}_{date.second}.log"
+        with open(name_of_file, "w") as f:
+            f.write(str(date))
+            print(f"{date} {name_of_file}")
+        sleep(1)
 
 
 if __name__ == "__main__":
