40c9fbe72c7aba676e7d200e692235fb570a011e
Yulia Bozhieva <bozhieva93@gmail.com>
2022-09-29 15:32:25 -0400

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        t = datetime.now()
+        ti = str(t).split()[1]
+        ti = ti.replace(":", "_")
+
+        file_name = "app-" + ti[0:8] + ".log"
+        with open(file_name, "w") as f:
+            f.write(str(t))
+        with open(file_name, "r") as f:
+            print(f.read(), file_name)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
