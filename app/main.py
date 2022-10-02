57c767af4f66d34e7e3185f4a42cb8764bf10764
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-10-03 00:48:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,12 +4,10 @@ from time import sleep
 
 def main():
     while True:
-        with open(f"app-{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}.log", "w+") as f:
+        with open(f"app-{datetime.now().hour}_"
+                  f"{datetime.now().minute}_"
+                  f"{datetime.now().second}.log", "w+") as f:
             text = f"{datetime.now()}"
             f.write(text)
             print(text, f.name)
         sleep(1)
-
-
-if __name__ == "__main__":
-    main()
