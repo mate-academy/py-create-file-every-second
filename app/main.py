13f61a249bc797c57befba9a3eb32aa5f6787c5f
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-03 14:37:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ import time
 def main():
     while True:
         full_time_now = datetime.now()
-        file_name = f"app-{full_time_now.hour}_{full_time_now.minute}_{full_time_now.second}.log"
+        file_name = f"app-{full_time_now.hour}_{full_time_now.minute}" \
+                    f"_{full_time_now.second}.log"
         with open(file_name, "w") as f:
             f.write(str(file_name))
         print(full_time_now, file_name)
