4cd1fd77e06a93d01d2eaa34ca1e1f11e64a4f6f
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-03 14:28:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,20 +3,9 @@ import time
 
 
 def main():
-    full_time_now = datetime.now()
-    time_split = str(full_time_now).split()
-    only_time_now = time_split[1]
-    hours_split = only_time_now.split(":")
-
-    hours = hours_split[0]
-    minutes = hours_split[1]
-
-    seconds_with_milliseconds = hours_split[2]
-    seconds_split = seconds_with_milliseconds.split(".")
-    seconds = seconds_split[0]
-
     while True:
-        file_name = f"app-{hours}_{minutes}_{seconds}.log"
+        full_time_now = datetime.now()
+        file_name = f"app-{full_time_now.hour}_{full_time_now.minute}_{full_time_now.second}.log"
         with open(file_name, "w") as f:
             f.write(str(file_name))
         with open(file_name, "r") as f:
