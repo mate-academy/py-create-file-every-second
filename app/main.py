b786213ca19a76e6236e3b172732f5412a1c9c33
Mariia Movchan <mariia.movchan.m@gmail.com>
2022-10-04 01:32:51 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
-def main():
-    now = datetime.now()
-    file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
 
+def main():
     while True:
+        now = datetime.now()
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(file_name, "w") as f:
             f.write(str(now))
 
-        print(str(now) + file_name)
+        print(str(now) + " " + file_name)
         sleep(1)
 
 
