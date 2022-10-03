e2761e2f006a138d8de8bc7e04dcf8001e970fda
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-03 14:40:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
-import time
+from time import sleep
 
 
 def main():
     while True:
-        full_time_now = datetime.now()
-        file_name = f"app-{full_time_now.hour}_{full_time_now.minute}" \
-                    f"_{full_time_now.second}.log"
-        with open(file_name, "w") as f:
-            f.write(str(file_name))
-        print(full_time_now, file_name)
-        time.sleep(1)
+        my_time = str(datetime.now()).split(".")
+        prep_file = f'app-{my_time[0].split()[1]}.log'.replace(":", "_")
+        with open(prep_file, "w") as file:
+            file.write(f'{datetime.now()}')
+        print(f'{datetime.now()} {prep_file}')
+        sleep(1)
 
 
 if __name__ == "__main__":
