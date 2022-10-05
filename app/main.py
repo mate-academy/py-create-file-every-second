9a6be568c9ef371c1935b6a698de6b4d031e81f4
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2022-10-05 13:46:27 +0300

    Update requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> datetime:
+    current_time = datetime.now()
+    while True:
+        sleep(1)
+        hours = current_time.hour
+        minutes = current_time.minute
+        seconds = current_time.second
+        with open(f"app-{hours}_{minutes}_{seconds}.log", "w") as f:
+            f.write(f"{current_time}")
+        hh = f"{current_time} app-{hours}_{minutes}_{seconds}.log"
+        print(hh)
+        current_time = datetime.now()
 
 
 if __name__ == "__main__":
