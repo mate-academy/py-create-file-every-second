3693115a6c77592f28fcf2398ede497ba6eccc38
LavrykN <nlavryk@kaaiot.io>
2022-10-10 17:10:06 +0300

    Create file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        time_now = datetime.now()
+        hours = time_now.time().hour
+        minutes = time_now.time().minute
+        seconds = time_now.time().second
+        file_name = f"app-{hours}_{minutes}_{seconds}.log"
+        with open(file_name, "w") as f:
+            f.write(str(time_now))
+        print(f"{str(time_now)} {str(file_name)}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
