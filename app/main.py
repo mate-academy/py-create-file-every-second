a995cb95a50b01a1bf6a3789e3542ba6fc59d114
Oleh Chornyi <ochernyi04@gmail.com>
2022-11-18 23:06:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        time_now = datetime.now()
+        name = f"app-{time_now.hour}_{time_now.minute}_{time_now.second}.log"
+        with open(name, "w") as file:
+            file.write(f"{time_now}")
+            print(time_now, name)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
