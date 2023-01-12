390e58bc1bacd46ab0cc3bd9b2ef114dd22f3969
Serhii Skivka <skivkas.v@gmail.com>
2023-01-12 06:48:53 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def main() -> None:
         current_time = datetime.now()
         file_name = f"app-{current_time.hour}_{current_time.minute}_" \
                     f"{current_time.second}.log"
-        with open(file_name, "w") as f:
-            f.write(str(current_time))
+        with open(file_name, "w") as ff:
+            ff.write(str(current_time))
             print(current_time, file_name)
         sleep(1)
 
