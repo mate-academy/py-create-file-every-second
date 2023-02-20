12e8d0b0af31e474165205f785a418917ce53036
name <ksyusha0kubay@gmail.com>
2023-02-20 16:22:15 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        current_time = datetime.now()
+        file_name = str(current_time)[11:13] + "_"
+        file_name += str(current_time)[14:16] + "_" + str(current_time)[17:19]
+
+        with open(f"app-{file_name}.log", "w") as f:
+            f.write(str(current_time))
+            print(str(current_time), f"app-{file_name}.log")
+        sleep(1)
 
 
 if __name__ == "__main__":
