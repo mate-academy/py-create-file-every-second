bd6f8527e60de7c405a92d3ce0ae21a4432c91eb
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-20 15:44:07 +0200

    Edited

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ from datetime import datetime
 def main() -> None:
     while True:
         time_now = datetime.now()
-        time_stamp = time_now.strftime("%Y-%m-%d %H:%M:%S.%f")
+        time_stamp = time_now.strftime("%Y-%m-%d %H:%M:%S")
         file_name = (f"app-{time_now.hour}_{time_now.minute}"
                      f"_{time_now.second}.log")
 
         with open(file_name, "w") as file:
             file.write(time_stamp)
 
-        print(f"Created file {file_name} at {time_stamp}")
+        print(f"{time_stamp} {file_name}")
         time.sleep(1)
 
 
