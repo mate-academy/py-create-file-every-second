7082b0100dd156e2d3d8f0591cd04e9964cac4b3
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-04-01 13:02:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        name_of_file = \
+            f"app-{now.strftime('%H')}_" \
+            f"{now.strftime('%M')}_{now.strftime('%S')}.log"
+        with open(name_of_file, "w") as f:
+            f.write(f"{now.strftime('%Y')}-{now.strftime('%m')}-"
+                    f"{now.strftime('%d')} {now.strftime('%H')}:"
+                    f"{now.strftime('%M')}:{now.strftime('%S')}")
+            time.sleep(1)
+        with open(name_of_file, "r") as f:
+            text = f.read()
+        print(text, name_of_file)
 
 
 if __name__ == "__main__":
