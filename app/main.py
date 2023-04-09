fe6326aa84c7ef0a93576221d6ecb6830e514809
Tatasha <NatashaLeonidovna319@gmail.com>
2023-04-10 00:37:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
+from datetime import datetime
+from typing import TextIO
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    class FileManager:
+        def __init__(self, filename: str, mode: str) -> None:
+            self.filename = filename
+            self.mode = mode
+
+        def __enter__(self) -> TextIO:
+            self.file = open(self.filename, self.mode)
+            return self.file
+
+        def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
+            self.file.close()
+
+    while True:
+        h_now = datetime.now().hour
+        min_now = datetime.now().minute
+        sec_now = datetime.now().second
+        time_now = datetime.now()
+        file_name = f"app-{h_now}_{min_now}_{sec_now}.log"
+        with FileManager(file_name, "w") as f:
+            f.write(str(time_now))
+        print(time_now, file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
