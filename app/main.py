931dc79372d782ff9f49128fa41b7459c9d6199e
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-11 11:37:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
 
 
 def main() -> None:
     current_date = datetime.now()
     formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S.%f")
-    file_name = "app-" + str(current_date.strftime("%H_%M_%S")) + ".log"
+    file_name = "app-" + current_date.strftime("%H_%M_%S") + ".log"
     with open(file_name, "a") as f:
         f.write(formatted_datetime)
 
