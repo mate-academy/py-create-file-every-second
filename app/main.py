711295d2b8397484a2e99755a287e404a2e81fcf
Oksana Makieieva <oksana.zhornyk@gmail.com>
2023-05-17 14:11:59 +0200

    py-create-file-every-second-v1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        file_name = (
+            f"app-{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}.log"
+        )
+        with open(file_name, "a") as file:
+            file.write(str(datetime.now()))
+        print(datetime.now(), file_name)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
