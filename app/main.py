c5dbb0ae9fb9910fb199d165ed7e3fbf1e3336be
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-19 19:43:29 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
 def main() -> None:
     while True:
-        file_name = "app-" + datetime.today().strftime("%H_%M_%S") + ".log"
+        file_name = "app-" + datetime.now().strftime("%H_%M_%S") + ".log"
         with open(file_name, "a") as file_date:
             file_date.write(str(datetime.now()))
         print(f"{datetime.now()} {file_name}")
