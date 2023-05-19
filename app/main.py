4ce77d163450c95cfe350a254080976dee80d434
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-19 19:31:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        file_name = "app-" + datetime.today().strftime("%H_%M_%S")+".log"
+        with open(file_name, "a") as file_date:
+            file_date.write(str(datetime.now()))
+        print(f"{datetime.now()} {file_name}")
+        sleep(1)
 
 
 if __name__ == "__main__":
