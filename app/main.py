d90b48785cbbc07b466de997efd519a441d04964
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-05-19 19:35:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
-        file_name = "app-" + datetime.today().strftime("%H_%M_%S")+".log"
+        file_name = "app-" + datetime.today().strftime("%H_%M_%S") + ".log"
         with open(file_name, "a") as file_date:
             file_date.write(str(datetime.now()))
         print(f"{datetime.now()} {file_name}")
