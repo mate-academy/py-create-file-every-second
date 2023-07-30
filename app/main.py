f4f6ad4605b7719ab3083464900f876afaa909cd
Vladyslav Shepilov <shepsterya@gmail.com>
2023-07-30 17:33:02 +0300

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from datetime import datetime
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
         f_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
         with open(f_name, "w") as file:
             file.write(timestamp)
             print(f"{timestamp} {f_name}")
