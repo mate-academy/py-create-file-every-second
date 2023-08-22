8c1c45fd6fcf41434207cb9b7bf0cbdef1b22bf4
oreny <orenycdota@gmail.com>
2023-08-22 16:45:00 +0300

    Test Push

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    now = datetime.now()
+    name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+    with open(name, "w") as f:
+        f.write(str(now))
+        print(now)
+        print(name)
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        sleep(1)
