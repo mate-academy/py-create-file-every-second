60b47d50f5da295a80f72a88b575906f9bf0a429
oreny <orenycdota@gmail.com>
2023-08-22 16:48:31 +0300

    Solved Push

--- app/main.py
+++ app/main.py
@@ -3,15 +3,14 @@ from time import sleep
 
 
 def main() -> None:
-    now = datetime.now()
-    name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-    with open(name, "w") as f:
-        f.write(str(now))
-        print(now)
-        print(name)
+    while True:
+        now = datetime.now()
+        name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        with open(name, "w") as f:
+            f.write(str(now))
+            print(now, name)
+        sleep(1)
 
 
 if __name__ == "__main__":
-    while True:
-        main()
-        sleep(1)
+    main()
