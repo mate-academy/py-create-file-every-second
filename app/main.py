ee0898c912de2cb3151d09c6dac8fedf030953a6
Oleh Bohatov <obohatov@gmail.com>
2023-08-25 17:19:08 +0200

    Solution with suggested by mentor changes

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ from time import sleep
 
 def main() -> None:
     while True:
-        file_name = (f"app-{datetime.now().hour}_"
-                     f"{datetime.now().minute}_"
-                     f"{datetime.now().second}.log")
+        file_name = (
+            f"app-{datetime.now().hour}_"
+            f"{datetime.now().minute}_"
+            f"{datetime.now().second}.log"
+        )
         with open(file_name, "w") as f:
             f.write(f"{datetime.now()}")
         print(f"{datetime.now()} {file_name}")
