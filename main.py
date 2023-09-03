9ebc2fb039d34ef81823e6d40e0636b1802757ce
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-09-03 10:59:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import time
 
 def main() -> None:
     while True:
-        current_time = datetime.now()
-        file_name = f"app-{current_time.hour}" \
-                    f"_{current_time.minute}_{current_time.second}.log"
+        while True:
+            current_time = datetime.now()
+            file_name = (f"app-{current_time.hour}_"
+                         f"{current_time.minute}_"
+                         f"{current_time.second}.log")
+            timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-        with open(file_name, "w") as file:
-            file.write(str(current_time))
+            with open(file_name, "w") as file:
+                file.write(timestamp)
 
-        print(f"{current_time} {file_name}")
-        time.sleep(1)
+            print(f"{timestamp} {file_name}")
+
+            time.sleep(1)
 
 
 if __name__ == "__main__":
+    main()
