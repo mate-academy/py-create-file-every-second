cf5d06da47551dda8e607588fb00635138813cc8
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-09-03 10:54:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
-from time import sleep
+from datetime import datetime
+import time
 
 
 def main() -> None:
-        while True:
-        hours = datetime.now().hour
-        minutes = datetime.now().minute
-        seconds = datetime.now().second
-        with open(f"app-{hours}_{minutes}_{seconds}.log", "w") as file:
-            file.write(str(datetime.now()))
-            print(str(datetime.now()), file.name)
-        sleep(1)
+    while True:
+        current_time = datetime.now()
+        file_name = f"app-{current_time.hour}" \
+                    f"_{current_time.minute}_{current_time.second}.log"
+
+        with open(file_name, "w") as file:
+            file.write(str(current_time))
+
+        print(f"{current_time} {file_name}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
