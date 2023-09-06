7818563759d808835075d2fa8a26da722737fa54
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-06 19:10:00 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,12 +3,13 @@ import time
 
 while True:
     current_time = datetime.now()
-    file_name = f'app-{current_time.hour}_{current_time.minute}_{current_time.second}.log'
-    file_content = current_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+    file_name = f"app-{current_time.hour}_" \
+                f"{current_time.minute}_{current_time.second}.log"
+    file_content = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
 
-    with open(file_name, 'w') as file:
+    with open(file_name, "w") as file:
         file.write(file_content)
 
-    print(f'Created file {file_name}')
+    print(f"Created file {file_name}")
 
     time.sleep(1)
