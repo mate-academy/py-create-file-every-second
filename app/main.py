1baf59a88f9142225dcb4886ed161f4b436165fb
Антон Пономаренко <pda130507@gmail.com>
2023-09-07 12:20:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,14 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
-    all_times = ""
+def main() -> None:
     while True:
         current_time = datetime.now()
         file_name = f"app-{current_time.strftime('%H_%M_%S')}.log"
         with open(file_name, "w") as file:
             file.write(str(current_time))
-            all_times += f"{current_time} {file_name} "
             sleep(1)
-        print(all_times)
+        print(current_time, file_name)
 
 
 if __name__ == "__main__":
