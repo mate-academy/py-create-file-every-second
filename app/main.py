4a31d98e470650b7291c11026f9c05182c778c9b
Pobedilda <playfog3@gmail.com>
2023-09-16 15:43:31 +0300

    fix error flake8

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main() -> None:
     while True:
         current_time = datetime.now()
         time_stamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
-        file_name = f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log"
+        file_name = f"app-{current_time.hour}" \
+                    f"_{current_time.minute}" \
+                    f"_{current_time.second}.log"
         with open(file_name, "w") as file:
             file.write(str(current_time))
         print(f"{time_stamp} {file_name}")
