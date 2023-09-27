dfe9aa8ae6d90f92c7c75b7c3c8022b83580102b
Vadym Tiron <tironvadim1@gmail.com>
2023-09-27 16:50:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def creating_f() -> None:
+    date = datetime.now()
+
+    file_name = f"app-{date.hour}_{date.minute}_{date.second}.log"
+
+    timestamp = (f"{date.year}-{date.month}-{date.day} {date.hour}:"
+                 f"{date.minute}:{date.second}.{date.microsecond}")
+
+    with open(file_name, "w") as file:
+        file.write(timestamp)
+
+
+def main() -> None:
+    while True:
+        creating_f()
+        sleep(1)
 
 
 if __name__ == "__main__":
