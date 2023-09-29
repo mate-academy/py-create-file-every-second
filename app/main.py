30155b1619788d875070c9450c4fb52aea4dcb60
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-09-29 19:41:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,16 +3,17 @@ import time
 
 
 def main() -> None:
-    now = datetime.now()
-    hours = now.hour
-    minutes = now.minute
-    seconds = now.second
+    while True:
+        now = datetime.now()
+        hours = now.hour
+        minutes = now.minute
+        seconds = now.second
 
-    formatted_timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-    with open(f"app-{hours}_{minutes}_{seconds}.log", "a") as f:
-        f.write(formatted_timestamp)
-        time.sleep(1)
-        print(f"{formatted_timestamp} app-{hours}_{minutes}_{seconds}.log")
+        formatted_timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open(f"app-{hours}_{minutes}_{seconds}.log", "a") as f:
+            f.write(formatted_timestamp)
+            time.sleep(1)
+            print(f"{formatted_timestamp} app-{hours}_{minutes}_{seconds}.log")
 
 
 if __name__ == "__main__":
