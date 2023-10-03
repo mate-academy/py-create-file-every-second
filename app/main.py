5d84e4a60102e8509282eedc78f2a807158d6bc1
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-03 11:50:26 +0300

    Implemented Create file every second function

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
+def main() -> None:
     while True:
         date_time = datetime.now()
-        file_name = f"app-{date_time.hour}_{date_time.minute}_{date_time.second}.log"
+        file_name = (f"app-{date_time.hour}"
+                     f"_{date_time.minute}"
+                     f"_{date_time.second}.log")
         with open(file_name, "w") as file:
             file.write(str(date_time))
         print(f"{date_time} {file_name}")
