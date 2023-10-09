fd48cc70ad5cc2f1a5c787ab5525d1a51b931078
Andrey Britov <abritov82@gmail.com>
2023-10-09 22:34:04 +0300

    Solusin_2

--- app/main.py
+++ app/main.py
@@ -2,22 +2,16 @@ from datetime import datetime
 from time import sleep
 
 
-def main():
-
-    # t = 0
+def main() -> None:
     while True:
-        date_now = datetime.now()
-        # print(date_now)
-        date_now = str(date_now).split(" ")
-        # print(date_now)
+        date_now_first = datetime.now()
+        date_now = str(date_now_first).split(" ")
         date_now = date_now[1]
-        # print(date_now)
         name = f"app-{date_now[0:2]}_{date_now[3:5]}_{date_now[6:8]}.log"
-        print(name)
-        with open(name, "a") as file:
-            file.write(f"{datetime.now} {name}")
+        with open(name, "w") as file:
+            file.write(str(date_now_first))
+        print(f"{date_now_first} {name}")
         sleep(1)
-        # t += 1
 
 
 if __name__ == "__main__":
