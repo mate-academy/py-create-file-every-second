1fd3787373ed994bbb9215553dfbe7f4d8f44abe
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-17 19:39:33 +0300

    Solution update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
-def main():
+
+def main() -> None:
     while True:
         curr_time = datetime.now().strftime("%H:%M:%S").split(":")
         file_time = f"app-{curr_time[0]}_{curr_time[1]}_{curr_time[2]}.log"
