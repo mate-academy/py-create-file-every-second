c866356726d6598f8475c50cace072b05b340695
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-17 19:35:20 +0300

    Solution update

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
-
+from time import sleep
 
 def main():
     while True:
-        time.sleep(1)
-        with open(f"app-{str(datetime.hour)}_{str(datetime.minute)}_{str(datetime.second)}", "w") as file:
-            file.write(f"{str(datetime.year)}-{str(datetime.month)}-{str(datetime.day)} {str(datetime.hour)}:{str(datetime.minute)}:{str(datetime.second)}")
-        print(f"{str(datetime.year)}-{str(datetime.month)}-{str(datetime.day)} {str(datetime.hour)}:{str(datetime.minute)}:{str(datetime.second)} app-app-{str(datetime.hour)}_{str(datetime.minute)}_{str(datetime.second)}")
+        curr_time = datetime.now().strftime("%H:%M:%S").split(":")
+        file_time = f"app-{curr_time[0]}_{curr_time[1]}_{curr_time[2]}.log"
+        with open(file_time, "w") as f:
+            current = datetime.now()
+            time = current.strftime("%Y-%m-%d %H:%M:%S")
+            f.write(str(time))
+            print(time + " " + file_time)
+        sleep(1)
 
 
 if __name__ == "__main__":
