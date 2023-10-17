883531359c56ee53c8296b96c776597af872d3b2
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-17 19:29:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        time.sleep(1)
+        with open(f"app-{str(datetime.hour)}_{str(datetime.minute)}_{str(datetime.second)}", "w") as file:
+            file.write(f"{str(datetime.year)}-{str(datetime.month)}-{str(datetime.day)} {str(datetime.hour)}:{str(datetime.minute)}:{str(datetime.second)}")
+        print(f"{str(datetime.year)}-{str(datetime.month)}-{str(datetime.day)} {str(datetime.hour)}:{str(datetime.minute)}:{str(datetime.second)} app-app-{str(datetime.hour)}_{str(datetime.minute)}_{str(datetime.second)}")
 
 
 if __name__ == "__main__":
