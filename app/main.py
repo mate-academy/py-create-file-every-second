982242a7cd3e9a398ae42a0c9954c194c6c86e01
Andrii Silenko <andrewformicr@gmail.com>
2023-10-18 22:21:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        create_time = datetime.now()
+        file_name = f"app-{create_time.hour}_{create_time.minute}_{create_time.second}.log"
+        file_content = (f"{create_time.year}-{create_time.month}-{create_time.day} "
+                        f"{create_time.hour}:{create_time.minute}:{create_time.second}")
+        with open(file_name, "w") as f:
+            f.write(file_content)
+
+        print(f"{file_content} {file_name}")
+        sleep(1)
 
 
 if __name__ == "__main__":
