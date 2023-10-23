b181b3d75923e5f7727b6a8dd7776f90c8267949
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-10-24 00:43:44 +0300

    Added return type annotation for main function.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         current_time = datetime.now()
         hours = f"{current_time.hour:02}"
