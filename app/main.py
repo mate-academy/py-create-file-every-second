2c5a296f1d2e6153261853b9cb716fa58718da3a
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-09 12:59:04 +0200

    Created file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def create_file() -> str:
+    current_date = datetime.now()
+    hours = current_date.hour
+    minutes = current_date.minute
+    seconds = current_date.second
+    file_name = f"app-{hours}_{minutes}_{seconds}.log"
+    content = str(current_date)
+    with open(file_name, "w") as f:
+        f.write(content)
+    print(f"{current_date} {file_name}")
+
+
+def main() -> None:
+    while True:
+        create_file()
+        sleep(1)
 
 
 if __name__ == "__main__":
