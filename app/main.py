695f7cd68b93592f9527036a4d29e5cbe931a59d
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-16 14:44:16 +0200

    feat: almost done

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        date = datetime.now()
+        hours = date.strftime("%H")
+        minutes = date.strftime("%M")
+        seconds = date.strftime("%S")
+        relative_path = f"app-{hours}_{minutes}_{seconds}.log"
+        file_path = os.path.join(script_dir, relative_path)
+        with open(file_path, "w") as f:
+            f.write(str(date))
+            sleep(1)
 
 
 if __name__ == "__main__":
