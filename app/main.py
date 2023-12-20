7615017c2f7a07e010e09a17e8e35f802918a7a6
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-12-20 18:45:08 +0200

    fix: small errors

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
-import os
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def main():
+def main() -> None:
     while True:
         date = datetime.now()
         hours = date.strftime("%H")
         minutes = date.strftime("%M")
         seconds = date.strftime("%S")
+        hours = date.hour
+        minutes = date.minute
+        seconds = date.second
         file_name = f"app-{hours}_{minutes}_{seconds}.log"
-        file_path = os.path.join(script_dir, file_name)
-        with open(file_path, "w") as f:
+        with open(file_name, "w") as f:
             f.write(str(date))
             print(str(date), file_name)
             sleep(1)
