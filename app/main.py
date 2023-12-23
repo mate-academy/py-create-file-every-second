5b2bb23f3ac44330532df3c90e55a4a773f2cae0
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-12-23 03:57:11 +0200

    Corrections

--- app/main.py
+++ app/main.py
@@ -6,13 +6,9 @@ def main() -> None:
     while True:
         current_date_time = datetime.now()
         with open(
-                f"app-{
-                current_date_time.hour
-                }_{
-                current_date_time.minute
-                }_{
-                current_date_time.second
-                }.log", "w"
+                f"app-{current_date_time.hour}_"
+                f"{current_date_time.minute}_"
+                f"{current_date_time.second}.log", "w"
         ) as file:
             file.write(str(current_date_time))
             print(f"{current_date_time} "
