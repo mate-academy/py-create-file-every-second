f9a610c12e73e20b5079f163b5656919107066d5
Nataliia <natali4.kucher55@gmail.com>
2024-01-14 14:09:36 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    try:
+        while True:
+            time = datetime.now()
+            file_name = f"app-{time.hour}_{time.minute}_{time.second}.log"
+
+            content = time.strftime("%Y-%m-%d %H:%M:%S")
+
+            with open(file_name, "a") as file:
+                file.write(content)
+
+            print(f"{content} {file_name}")
+
+            sleep(1)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
