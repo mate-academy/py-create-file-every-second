46831f1b97bbf0209d5e97c07769e4d568c92b32
yes <yes@yess-MacBook-Pro.local>
2024-01-18 02:30:45 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -7,13 +7,15 @@ import sys
 def main():
     os.chdir(os.path.dirname(sys.argv[0]))
     while True:
-        current_datetime = datetime.now()
-        file_name = f"app-{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}.log"
-        new_file = open(file_name, "w")
-        new_file.write(str(datetime.now()))
+        d = datetime.now()
+        h, m, s = d.hour, d.minute, d.second
+        name = f"app-{h}_{m}_{s}.log"
+        new_file = open(name, "w")
+        new_file.write(str(d))
         new_file.close()
-        print(f"{datetime.now()} {file_name}")
+        print(f"{d} {name}")
         time.sleep(1)
-        
+
+
 if __name__ == "__main__":
     main()
