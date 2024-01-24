1b553a77ee49378f404265d3fd36992f107d3e39
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-01-24 13:03:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from time import sleep
 def main() -> None:
     while True:
         time_ins = datetime.now()
-        file_name = f"app-{time_ins.time().strftime("%H_%M_%S")}.log"
+        file_name = f'app-{time_ins.time().strftime("%H_%M_%S")}.log'
         with open(file_name, "w") as f:
             f.write(str(time_ins))
         print(time_ins, end="")
