863397b2d2a566b5e81c750e036404d2e4fa6cb8
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-24 16:58:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
+from datetime import datetime
 
 
-def main():
-    # write your code here
+def main() -> None:
+    while True:
+        try:
+            now = datetime.now()
+            hours, minutes, seconds = str(now.time()).split(":")
+            file_name = (f"app-{hours}_"
+                         f"{minutes}_"
+                         f"{seconds}")
+            with open(f"{file_name}.log", "w") as file:
+                file.write(f"{str(now)}")
+            print(f"{now} {file_name}.log")
+            time.sleep(1)
+        except KeyboardInterrupt:
+            raise
     pass
 
 
