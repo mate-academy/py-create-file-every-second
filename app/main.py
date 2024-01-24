ff8637a7462d586c5800dd1c55c4828d8e2fd988
Serhii Buryk <sergmarten21@gmail.com>
2024-01-24 15:46:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ from datetime import datetime
 def main() -> None:
     while True:
         try:
+            formated_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             file_name = (f"app-{datetime.now().hour}_"
                          f"{datetime.now().minute}_"
                          f"{datetime.now().second}")
             with open(f"{file_name}.log", "w") as file:
-                file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+                file.write(f"{formated_time}")
             print(f"{datetime.now()} {file_name}.log")
             time.sleep(1)
         except KeyboardInterrupt:
