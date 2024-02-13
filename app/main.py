cd638cb7e1c84c49fe96aad7c35a544d31787759
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-02-13 21:46:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,29 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def create_file() -> None:
+    current_time = datetime.now()
+    file_name = (f"app-{current_time.hour:02d}_"  # noqa
+                 f"{current_time.minute:02d}_"  # noqa
+                 f"{current_time.second:02d}.log")  # noqa
+    file_content = current_time.strftime(
+        "%Y-%m-%d %H:%M:%S.%f"
+    )[:-7].rstrip("0")
+
+    with open(file_name, "a") as f:
+        f.write(file_content)
+
+    print(f"{current_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-7]} {file_name}")
+
+
+def main() -> None:
+    try:
+        while True:
+            create_file()
+            sleep(1)
+    except KeyboardInterrupt:
+        raise
 
 
 if __name__ == "__main__":
