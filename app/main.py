dfe40b96341e4174037226bce8ca3695d8fb0bb4
StanislavBlashchuk <stanislav.blashchuk@cinarra.com>
2024-02-23 18:34:12 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+
+    while True:
+        current_time = datetime.now()
+
+        time_stamp = str(current_time).split()[1]
+        split_time_stamp = time_stamp.split(":")
+
+        hours = split_time_stamp[0]
+        minutes = split_time_stamp[1]
+        seconds = split_time_stamp[2].split(".")[0]
+
+        with open(f"app-{hours}_{minutes}_{seconds}.log", "w") as f:
+            f.write(str(current_time))
+
+        print(f"{current_time} {f.name}")
+
+        sleep(1)
 
 
 if __name__ == "__main__":
