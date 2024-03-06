ef19367b2f43e4c781d11d86da5a5c327eb25078
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-03-06 09:54:09 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 import time
-import os
 
 
-def main():
+def main() -> None:
     try:
         while True:
             current_time = datetime.now()
             file_name = f"app-{current_time.strftime('%H_%M_%S')}.log"
-            file_content = current_time.strftime('%Y-%m-%d %H:%M:%S')
+            file_content = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
             with open(file_name, "w") as f:
                 f.write(file_content)
