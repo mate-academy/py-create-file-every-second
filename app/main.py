6f2ff9c78982b5f188de95bc8aadb4527cda0639
Oles Shulzhenko <simplemanua@gmail.com>
2024-03-07 18:40:34 +0200

    'Corected'

--- app/main.py
+++ app/main.py
@@ -1,22 +1,19 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 import time
+from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
 def main() -> None:
-    current_time = datetime.now()
-    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
-    file_name = (f"app-{current_time.hour}_{current_time.minute}_"
-                 f"{current_time.second}.log")
-    with open(file_name, "w") as f:
-        f.write(formatted_time)
+    while True:
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-7]
+        file_name = (f"app-{current_time.hour}_{current_time.minute}_"
+                     f"{current_time.second}.log")
+        with open(file_name, "w") as f:
+            f.write(formatted_time)
 
-    with open(file_name, "r") as f:
-        print(f.read())
+        print(f"{formatted_time} {file_name}")
 
-
-while True:
-    main()
-    time.sleep(1)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
