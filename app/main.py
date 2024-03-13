741ce4547f853129a621137bfa331ef17639d200
Misha Goryn <mgoryn68@gmail.com>
2024-03-13 22:24:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from time import sleep
 def main() -> None:
     while True:
         current_time = datetime.now()
-        file_name = f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log"
+        file_name = f"app-{
+            current_time.hour}_{current_time.minute}_{current_time.second}.log"
         file_content = str(current_time)
 
         with open(file_name, "w") as file:
