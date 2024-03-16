2563b3e9d656fd7f977edcea9fc25cd2f2b225ac
Artom <artem.umantsev@gmail.com>
2024-03-16 17:23:58 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main() -> None:
     try:
         while True:
             current_time = datetime.now()
-            file_name = (f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log")
+            file_name = (f"app-{current_time.hour}_"
+                         f"{current_time.minute}_"
+                         f"{current_time.second}.log")
             with open(file_name, "w") as file:
                 file.write(str(current_time))
                 print(f"{current_time} {file_name}")
