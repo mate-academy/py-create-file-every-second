f57a3bb3d22ac0fceec51646b0521ab949c680c2
Futilite <marketesh@gmail.com>
2024-03-24 20:44:00 +0200

    FileEvSecond_1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def create_file() -> None:
+    now = datetime.now()
+    filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+    with open(filename, "w") as f:
+        f.write(str(now))
+    print(now, filename)
+
+
+def main() -> None:
+    while True:
+        create_file()
+        sleep(1)
 
 
 if __name__ == "__main__":
