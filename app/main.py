d0bd3f134bea2a3ae4efe8bb616b2d10c153d4e6
Vitalii <warcied1@gmail.com>
2024-03-25 00:07:59 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         curr_date = datetime.now()
-        filename = f"app-{curr_date.hour}_{curr_date.minute}_{curr_date.second}.log"
+        filename = (f"app-{curr_date.hour}_"
+                    f"{curr_date.minute}_{curr_date.second}.log")
         with open(filename, "w+") as file:
             file.write(str(curr_date))
         print(f"{str(curr_date)} {filename}")
