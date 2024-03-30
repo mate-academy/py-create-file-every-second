b3057a6e9ce6b027dce055c7de194b0ba490a94a
Shershen Alexandra <hornet240204@gmail.com>
2024-03-30 15:25:27 +0200

    Fix logic of working in the code

--- app/main.py
+++ app/main.py
@@ -3,17 +3,17 @@ from time import sleep
 
 
 def main() -> None:
-    current_time = datetime.now()
-    file_name = (f"app-{current_time.hour}_{current_time.minute}_"
-                 f"{current_time.second}.log")
-    file_content = str(current_time)
-
     while True:
+        current_time = datetime.now()
+        file_name = (f"app-{current_time.hour}_{current_time.minute}_"
+                     f"{current_time.second}.log")
+        file_content = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
         output_file = open(file_name, "w")
         output_file.write(file_content)
 
-        print(f"{current_time} {file_name}")
         sleep(1)
+        print(f"{file_content} {file_name}")
 
 
 if __name__ == "__main__":
