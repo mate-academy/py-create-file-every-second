0e01b2a1ed47a39e79b8216027608102871a3eb3
Liliia <furman06228@gmail.com>
2024-04-03 07:54:33 +0200

    Solution 2.0.

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ import time
 
 def main() -> None:
     while True:
-            now = datetime.now()
-            filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
-            timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-            with open(filename, "w") as f:
-                f.write(timestamp)
+        now = datetime.now()
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open(filename, "w") as f:
+            f.write(timestamp)
 
-            print(timestamp, filename)
+        print(timestamp, filename)
 
-            time.sleep(1)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
