01d7ba16bafdaf3ba7f9512fadcc78b4a34f8dc9
haldaniko <haldaniko@gmail.com>
2024-04-09 00:14:20 +0200

    solution 2

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from datetime import datetime
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
-        with open(f"app-{datetime.now().hour}_{datetime.now().minute}_{datetime.now().second}.log", "w") as file:
+        with open(f"app-{datetime.now().hour}_"
+                  f"{datetime.now().minute}_"
+                  f"{datetime.now().second}.log", "w") as file:
             file.write(str(datetime.now()))
             print(datetime.now(), file.name)
             sleep(1)
