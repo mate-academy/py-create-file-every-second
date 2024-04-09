8ef9a88a79d91268af248fdab0ad04de15bfa9df
Yaroslav Ivanov <165368790+nrjss67@users.noreply.github.com>
2024-04-09 17:22:41 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
 
     while True:
-        time_now = (datetime.now().hour, datetime.now().minute, datetime.now().second)
+        time_now = (datetime.now().hour,
+                    datetime.now().minute,
+                    datetime.now().second)
         file_name = "app-{}_{}_{}.log".format(*time_now)
         with open(file_name, "a") as file:
             file.write(str(datetime.now()))
