759bebbb1ae49d427b220469558ae78e46923399
Kate <katrusja.ko@gmail.com>
2024-04-26 15:16:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ import time
 def main() -> None:
     while True:
         time_now = datetime.now()
-        with open(f"app-{time_now.hour}_{time_now.minute}_{time_now.second}.log", "a") as f:
+        with open(f"app-{time_now.hour}_"
+                  f"{time_now.minute}_{time_now.second}.log", "a") as f:
             f.write(str(time_now))
-            print(str(time_now), f"app-{time_now.hour}_{time_now.minute}_{time_now.second}.log")
+            print(str(time_now), f"app-{time_now.hour}"
+                                 f"_{time_now.minute}_{time_now.second}.log")
         time.sleep(1)
 
 
