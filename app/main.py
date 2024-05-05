39ff33f97721aa46ed14f23a8a9762a6129a8199
Stekloduv <102047106+Stekloduv@users.noreply.github.com>
2024-05-05 10:32:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 import time
+from typing import Any
 
 
-def main():
+def main() -> Any:
     while True:
         current_time = datetime.now()
         time.sleep(1)
-        with open(
-            f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log", "w"
-        ) as f:
+        with open(f"app-{current_time.hour}_{current_time.minute}_\
+{current_time.second}.log", "w") as f:
             f.write(f"{current_time}")
 
 
