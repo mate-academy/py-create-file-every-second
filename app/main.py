7cc00cb0bd4d981df6a27c7184b74cd9ce8fce15
Stekloduv <102047106+Stekloduv@users.noreply.github.com>
2024-05-05 10:57:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ from typing import Any
 
 def main() -> Any:
     while True:
-        time.sleep(1)
         current_time = datetime.now()
-        timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
         filename_str = current_time.strftime("app-%H_%M_%S.log")
         with open(filename_str, "w") as f:
             f.write(timestamp_str)
         print(f"{timestamp_str} {filename_str}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    main() 
+    main()
