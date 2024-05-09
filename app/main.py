c3b1fdbc6412d8d6b15b583a7ff5bc8907ad3f38
Your_Name Your_Surname <your@email>
2024-05-10 00:15:49 +0300

    add Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def create_file() -> None:
+    now = datetime.now()
+    formatted_time = now.strftime("app-%H_%M_%S.log")
+
+    with open(formatted_time, "w") as f:
+        f.write(str(now))
+
+    print(f"{now} {formatted_time}")
+
+
+def main() -> None:
+    while True:
+        create_file()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
