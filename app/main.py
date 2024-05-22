399bbe4a07542d789415599294738b6eadabd940
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-05-22 23:26:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,20 +4,12 @@ import time
 
 def main() -> None:
     while True:
-        # Отримуємо поточний час
         now = datetime.now()
-
-        # Форматуємо ім'я файлу
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
 
-        # Записуємо час у файл
         with open(file_name, "w") as file:
             file.write(now.strftime("%Y-%m-%d %H:%M:%S.%f"))
-
-        # Виводимо інформацію про створення файлу
         print(f'{now.strftime("%Y-%m-%d %H:%M:%S.%f")} {file_name}')
-
-        # Затримка на 1 секунду
         time.sleep(1)
 
 
