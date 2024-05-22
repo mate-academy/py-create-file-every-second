a2d95b090cf996be1cdf1331188e6eab4e10fa14
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-05-22 23:11:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+import time
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        # Отримуємо поточний час
+        now = datetime.now()
+
+        # Форматуємо ім'я файлу
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+
+        # Записуємо час у файл
+        with open(file_name, "w") as file:
+            file.write(now.strftime("%Y-%m-%d %H:%M:%S.%f"))
+
+        # Виводимо інформацію про створення файлу
+        print(f"{now.strftime("%Y-%m-%d %H:%M:%S.%f")} {file_name}")
+
+        # Затримка на 1 секунду
+        time.sleep(1)
 
 
-if __name__ == "__main__":
-    main()
+main()
