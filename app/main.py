f0ec84b8a50eba2d38306f2eaef8943118cb898c
Alex Bul <tarasov57228@gmail.com>
2024-06-03 20:04:59 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main() -> None:
     while True:
         dt_now = datetime.now()
         file_name = f"app-{dt_now.hour}_{dt_now.minute}_{dt_now.second}.log"
-        file_write = f"{dt_now.strftime("%Y-%m-%d %H:%M:%S")}"
+        file_write = dt_now.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_name, "w") as f:
             f.write(file_write)
         print(file_write, file_name)
