8d3a14464feeb2c91196ec2eb0e0e13fcf222017
jpdreamthug <lentorilp@gmail.com>
2024-06-03 15:07:15 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main() -> None:
     while True:
 
         current_time = datetime.now()
-        file_name = f"app-{current_time.strftime("%H_%M_%S")}.log"
+        file_name = f"app-{current_time.strftime('%H_%M_%S')}.log"
         timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_name, "w") as f:
             f.write(timestamp)
