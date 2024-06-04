76cf6fc0f1677cdaf2bcfdd9da1b3772ad3ff336
Sergii Semenets <serhey.semenets@gmail.com>
2024-06-04 12:57:59 +0300

    Solution some change 1

--- app/main.py
+++ app/main.py
@@ -3,10 +3,9 @@ import time
 
 
 def main() -> None:
-    now = datetime.now()
-    file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-
     while True:
+        now = datetime.now()
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(file_name, "w") as f:
             f.write(str(now))
             print(now, file_name)
