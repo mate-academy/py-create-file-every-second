8390be7fa08a84b0f8a508a303c52d402dac6254
unlie9 <unlie.devops9@gmail.com>
2024-06-04 16:16:37 +0300

    sol1

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def main() -> None:
     while True:
         now = datetime.now()
-        file_name = f"app-{now.strftime("%H_%M_%S")}.log"
+        file_name = f'app-{now.strftime("%H_%M_%S")}.log'
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_name, "w") as f:
             f.write(timestamp)
