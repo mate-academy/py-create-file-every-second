be8f18bf7d29d18947210e40b86ff325482979cf
Oleh Deshko <kochaminny@gmail.com>
2024-06-04 15:06:43 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         current_time = datetime.now()
-        file_name = f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log"
-        file_content = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
-
-        with open(file_name, 'w') as file:
+        file_name = (
+            f"app-{current_time.hour}_{current_time.minute}"
+            f"_{current_time.second}.log"
+        )
+        file_content = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        with open(file_name, "w") as file:
             file.write(file_content)
 
         print(f"{file_content} {file_name}")
