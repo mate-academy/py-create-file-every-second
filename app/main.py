7fc423b14b09a4a85aa8f10b419c908fc08be83f
timur <stukantimur811@gmail.com>
2024-06-09 23:09:30 +0300

    files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+import time
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        temp_time = str(datetime.now())
+        temp_file = (
+                f"app-{temp_time[11:19].split(":")[0]}_"
+                f"{temp_time[11:19].split(":")[1]}_"
+                f"{temp_time[11:19].split(":")[2]}.log"
+        )
+        with open(f"{temp_file}", "w") as file:
+            file.write(str(temp_time))
+        print(temp_time, temp_file)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
