5f6b5c05ecce8468ee3659beca14c2ffd62e588d
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-16 20:23:51 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -2,15 +2,17 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 import time
 
 
-
-def main():
+def main() -> None:
     while True:
         currnet_time = datetime.now()
 
-        file_name = f"app-{currnet_time.hour}_{currnet_time.minute}_{currnet_time.second}.log"
+        file_name = \
+            (f"app-{currnet_time.hour}"
+             f"_{currnet_time.minute}_"
+             f"{currnet_time.second}.log")
 
-        with open(file_name, 'w') as f:
-            timestamp = currnet_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+        with open(file_name, "w") as f:
+            timestamp = currnet_time.strftime("%Y-%m-%d %H:%M:%S.%f")
             f.write(timestamp)
 
         print(f"{timestamp} {file_name}")
