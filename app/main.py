839b5c5e8eae1bb33345967ac8aea9dec6216354
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-16 20:18:57 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
+
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        currnet_time = datetime.now()
+
+        file_name = f"app-{currnet_time.hour}_{currnet_time.minute}_{currnet_time.second}.log"
+
+        with open(file_name, 'w') as f:
+            timestamp = currnet_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+            f.write(timestamp)
+
+        print(f"{timestamp} {file_name}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
