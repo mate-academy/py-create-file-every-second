049dd9b52c65c1ef4f287eb2ef8d93dbc8f2b876
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-21 22:14:17 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,20 +2,20 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
-        file_name = f"app-{now.hour:02d}_{now.minute:02d}_{now.second:02d}.log"
 
-        timestamp_str = now.strftime(f"{now.year}-{now.month:02d}-{now.day:02d} "
-                                     f"{now.hour:02d}:{now.minute:02d}:{now.second:02d}.{now.microsecond:06d}")
+        timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
 
         with open(file_name, "w") as file:
-            file.write(f"{timestamp_str}\n")
+            file.write(f"{timestamp_str}")
 
         print(f"{timestamp_str} {file_name}")
 
         sleep(1)
+        file.close()
 
 
 if __name__ == "__main__":
