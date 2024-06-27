48d4dd4a53568d33cf34d32293e8942f934e69ee
Buchak Tamara <Buchak.Tamara@gmail.com>
2024-06-27 22:00:31 +0300

    add edits

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
-import time
+from time import sleep
 from datetime import datetime
 
 
 def main() -> None:
 
     while True:
+
         file_contents = datetime.now()
-        name = (
+        file_name = (
             f"app-{datetime.now().hour}_{datetime.now().minute}"
             f"_{datetime.now().second}.log"
         )
-        with open(name, "w") as f:
-            print(f"{file_contents} {name}")
+
+        with open(file_name, "w") as f:
             f.write(f"{file_contents}")
-        time.sleep(1)
+            print(f"{file_contents} {file_name}")
+
+        sleep(1)
 
 
 if __name__ == "__main__":
