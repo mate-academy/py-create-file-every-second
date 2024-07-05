35c9d04b6860aff2e76120849a58ea8349800b87
Anatoliy <an.kaminskij@gmail.com>
2024-07-05 09:59:30 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ from time import sleep
 def main() -> None:
     while True:
         current_time = datetime.now()
-        timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
-        file_name = current_time.strftime("app-%H_%M_%S.log")
+        timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        filename = current_time.strftime("app-%H_%M_%S.log")
 
-        with open(file_name, "w") as file:
+        with open(filename, mode="w") as file:
             file.write(timestamp)
 
-        print(f"{timestamp} {file_name}")
+        print(f"{timestamp} {filename}")
 
         sleep(1)
 
