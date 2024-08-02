7d88a5c6d60489ac845c0bca34f2cfdf24963c3c
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-02 18:15:45 +0300

    added changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
+
 def main() -> None:
     while True:
         date_now = datetime.now()
         current_time = str(date_now.time()).split(":")
-        file_name = f"app-{current_time[0]}_{current_time[1]}_{current_time[2][:2]}.log"
+        file_name = (f"app-{current_time[0]}_"
+                     f"{current_time[1]}_"
+                     f"{current_time[2][:2]}.log"
+                     )
         with open(file_name, "w") as new_file:
             new_file.write(f"{date_now}")
         with open(file_name, "r") as file:
