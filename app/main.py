c057d8ac762ef68273c63584eecae8812d01de99
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-04 19:50:57 +0300

    first try changed quotes

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ import time
 
 def main() -> None:
     while True:
-        file_name = f"app-{datetime.now().strftime('%Y%m%d-%H_%M_%S')}.log"
+        file_name = f'app-{datetime.now().strftime("%H_%M_%S")}.log'
 
         with open(file_name, "w") as f:
-            f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')} {file_name}")
+        print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {file_name}')
         time.sleep(1)
 
 
