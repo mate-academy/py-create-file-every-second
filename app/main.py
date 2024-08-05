d7dcedede1c6ec18733d511cecc428380ae83257
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-06 00:05:21 +0300

    first try

--- app/main.py
+++ app/main.py
@@ -6,19 +6,19 @@ def main() -> None:
 
     while True:
 
-        current = datetime.now()
-        current_hours = current.hour
-        current_minutes = current.minute
-        current_seconds = current.second
+        current_time = datetime.now()
+        current_hours = current_time.hour
+        current_minutes = current_time.minute
+        current_seconds = current_time.second
 
         with open(
             f"app-{current_hours}_{current_minutes}_{current_seconds}.log", "w"
         ) as new_file:
 
-            new_file.write(str(current))
+            new_file.write(str(current_time))
 
         print(
-            str(current),
+            str(current_time),
             f"app-{current_hours}_{current_minutes}_{current_seconds}.log"
         )
 
