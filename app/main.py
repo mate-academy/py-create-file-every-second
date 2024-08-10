36837f489d6a227bba137af9dd468835650d4d04
Oleksandr Vovk <165489096+VovkOL@users.noreply.github.com>
2024-08-10 17:29:06 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> str:
+    try:
+        while True:
+            time_now = datetime.now()
+            filename = (
+                f"app-{time_now.hour}_{time_now.minute}_"
+                f"{time_now.second}.log"
+            )
+            timestamp = (
+                f"{time_now.year}-{time_now.month}-{time_now.day} "
+                f"{time_now.hour}:{time_now.minute}:{time_now.second}"
+            )
+            with open(filename, "w") as file:
+                file.write(timestamp)
+            print(f"{timestamp} {filename}")
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
 
 
 if __name__ == "__main__":
