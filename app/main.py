7ec24297a688187426e02da625ad39fa8671401f
Arthur <avers.target@gmail.com>
2024-08-28 21:15:16 +0200

    feat: add logging functionality to main.py
    This commit adds logging functionality to the `main.py` file. It creates a new log file every second with the current timestamp as the file name. The log file contains the current datetime. This change improves the debugging and monitoring capabilities of the application.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        hours, minutes, seconds = (
+            datetime.now().hour,
+            datetime.now().minute,
+            datetime.now().second,
+        )
+        file_name = f"app-{hours}_{minutes}_{seconds}.log"
+        with open(file_name, "w") as f:
+            f.write(str(datetime.now()))
+            print(datetime.now(), file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
