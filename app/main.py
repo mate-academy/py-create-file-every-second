a1a9e472823fceb4f598af3d4e5ac8a68a953e14
poznianski <poznianski@gmail.com>
2024-09-20 23:01:04 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def create_file() -> None:
+    now = datetime.now().replace(microsecond=0)
+
+    filename = f"app-{now.strftime("%H_%M_%S")}.log"
+
+    content = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(filename, "w") as f:
+        f.write(content)
+
+    print(f"{content} {filename}")
+
+
+def main() -> None:
+    while True:
+        create_file()
+        sleep(1)
 
 
 if __name__ == "__main__":
