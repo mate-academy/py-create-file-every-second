816ade5adcd8d6a13c41fed3d76e4f9b7ff6f47f
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2024-09-25 12:55:11 +0300

    The f-string error has been fixed

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main() -> None:
 
     while True:
         current_date = datetime.now()
-        file_name = f"app-{current_date.strftime("%H_%M_%S")}.log"
+        file_name = f"app-{current_date.strftime('%H_%M_%S')}.log"
 
         with open(file_name, "w") as f:
             f.write(f"{current_date}")
