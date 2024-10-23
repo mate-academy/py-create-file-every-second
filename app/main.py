874d779c29220f4cc2b918f99f8470da49d65a61
srx <srx92@protonmail.com>
2024-10-23 15:40:11 +0300

    Solution fix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from time import sleep
 
 def main() -> None:
     while True:
-        file_name = f"app-{datetime.now().strftime("%H_%M_%S")}.log"
+        file_name = f'app-{datetime.now().strftime("%H_%M_%S")}.log'
         time_stamp = datetime.now()
         with open(file_name, "w") as log:
             log.write(str(time_stamp))
