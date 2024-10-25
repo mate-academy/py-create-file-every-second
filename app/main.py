2de041d40acc995e27a5c3c8b50eaedcaa487a63
Volodymyr Bilous <bilous19996@gmail.com>
2024-10-25 14:38:20 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        # Get the current timestamp
+        timestamp = datetime.now()
+
+        # Format file name as specifies
+        file_name = (f"app-{timestamp.hour}_{timestamp.minute}"
+                     f"_{timestamp.second}.log")
+
+        # Write timestamp to the file
+        with open(file_name, "w") as file:
+            file.write(str(timestamp))
+
+        # Print timestamp and file name to console
+        print(f"{timestamp} {file_name}")
+
+        # Wait for 1 second before creating the next file
+        sleep(1)
 
 
 if __name__ == "__main__":
