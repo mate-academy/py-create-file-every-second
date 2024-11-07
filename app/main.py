7055ee4982e89dc046c64fe903f861ba14074e74
fanni <fmalevych@zoolatech.com>
2024-11-07 13:07:32 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ def main() -> None:
     while True:
         timestamp = datetime.now()
         file_name = (f"app-{timestamp.hour}_"
-                     f"{timestamp.minute}_"
-                     f"{timestamp.second}.log")
+                     f"{timestamp.minute}_{timestamp.second}.log")
         with open(file_name, "w") as f:
             f.write(str(timestamp))
         print(f"{timestamp} {file_name}")
