086f03620b06ac3e34a7d41f0fa8e737836254ec
Anastasiia Khudyakova <sun14water16@gmail.com>
2024-11-15 14:54:40 +0200

    1 push

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
 def main():
-    # write your code here
-    pass
-
+    while True:
+        t = datetime.now().time()
+        d = datetime.now()
+        with open(f"app-{t.hour}_{t.minute}_{t.second}.log", "w") as f:
+            f.write(str(d))
+        print(d, f"app-{t.hour}_{t.minute}_{t.second}.log")
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
