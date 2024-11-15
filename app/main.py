0931ce9069642fa68850515b076a579e0effa5fc
Anastasiia Khudyakova <sun14water16@gmail.com>
2024-11-15 15:00:03 +0200

    3 push

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
+def main() -> None:
     while True:
-        t = datetime.now().time()
-        d = datetime.now()
-        with open(f"app-{t.hour}_{t.minute}_{t.second}.log", "w") as f:
-            f.write(str(d))
-        print(d, f"app-{t.hour}_{t.minute}_{t.second}.log")
+        time_now = datetime.now().time()
+        name = f"app-{time_now.hour}_{time_now.minute}_{time_now.second}.log"
+        date_now = datetime.now()
+        with open(name, "w") as f:
+            f.write(str(date_now))
+        print(date_now, name)
         time.sleep(1)
 
 
