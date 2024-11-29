4c114e0228d82dc8e9644739d97a63f79fa82355
Mazurkav <aleshkamazurkevich@gmail.com>
2024-11-29 18:27:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
+try:
+    while True:
+        # Get the current timestamp
+        current_time = datetime.now()
 
-def main():
-    # write your code here
-    pass
+        # Format the file name
+        file_name = f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log"
 
+        # Write the timestamp into the file
+        with open(file_name, "w") as file:
+            file.write(str(current_time))
 
-if __name__ == "__main__":
-    main()
+        # Print the timestamp and file name to the console
+        print(f"{current_time} {file_name}")
+
+        # Wait for 1 second
+        sleep(1)
+
+except KeyboardInterrupt:
+    print("\nProcess terminated by user.")
