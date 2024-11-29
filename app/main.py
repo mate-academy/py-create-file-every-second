d6898a24ac1d88694d6ee553c6e2ffc472b73272
Mazurkav <aleshkamazurkevich@gmail.com>
2024-11-29 18:35:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,27 +1,34 @@
 from datetime import datetime
 from time import sleep
 
-try:
-    while True:
-        # Get the current timestamp
-        current_time = datetime.now()
+def create_log_files(loop_limit=None):
+    """Create log files with a timestamp in an optional limited loop."""
+    try:
+        loop_count = 0
+        while loop_limit is None or loop_count < loop_limit:
+            # Get the current timestamp
+            current_time = datetime.now()
 
-        # Format the file name
-        file_name = (
-            f"app-{current_time.hour}_"
-            f"{current_time.minute}_"
-            f"{current_time.second}.log"
-        )
+            # Format the file name
+            file_name = (
+                f"app-{current_time.hour}_"
+                f"{current_time.minute}_"
+                f"{current_time.second}.log"
+            )
 
-        # Write the timestamp into the file
-        with open(file_name, "w") as file:
-            file.write(str(current_time))
+            # Write the timestamp into the file
+            with open(file_name, "w") as file:
+                file.write(str(current_time))
 
-        # Print the timestamp and file name to the console
-        print(f"{current_time} {file_name}")
+            # Print the timestamp and file name to the console
+            print(f"{current_time} {file_name}")
 
-        # Wait for 1 second
-        sleep(1)
+            # Wait for 1 second
+            sleep(1)
+            loop_count += 1
 
-except KeyboardInterrupt:
-    print("\nProcess terminated by user.")
+    except KeyboardInterrupt:
+        print("\nProcess terminated by user.")
+
+if __name__ == "__main__":
+    create_log_files()
