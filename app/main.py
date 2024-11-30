2ca5ad73288cb3638f495bebb207233257a0bf40
Valdyslav <magnetto54@gmail.com>
2024-11-30 09:39:47 +0100

    Solution_2

--- app/main.py
+++ app/main.py
@@ -3,19 +3,26 @@ from datetime import datetime
 
 
 def main() -> None:
-    dat_name = datetime.now()
-    while True:
-        time.sleep(1)
-        with open(f"app-{dat_name.strftime('%H')}"
-                  f"_{dat_name.strftime('%M')}"
-                  f"_{dat_name.strftime('%S')}.log", "a") as f:
-            f.write(f"{dat_name.strftime('%Y')}-"
-                    f"{dat_name.strftime('%m')}-"
-                    f"{dat_name.strftime('%d')} "
-                    f"{dat_name.strftime('%H')}:"
-                    f"{dat_name.strftime('%M')}:"
-                    f"{dat_name.strftime('%S')}."
-                    f"{dat_name.strftime('%f')}")
+    try:
+        while True:
+            dat_name = datetime.now()
+            time.sleep(1)
+            with open(f"app-{dat_name.strftime('%H')}"
+                      f"_{dat_name.strftime('%M')}"
+                      f"_{dat_name.strftime('%S')}.log", "w") as f:
+                f.write(f"{dat_name.strftime('%Y')}-"
+                        f"{dat_name.strftime('%m')}-"
+                        f"{dat_name.strftime('%d')} "
+                        f"{dat_name.strftime('%H')}:"
+                        f"{dat_name.strftime('%M')}:"
+                        f"{dat_name.strftime('%S')}")
+            with open(f"app-{dat_name.strftime('%H')}"
+                      f"_{dat_name.strftime('%M')}"
+                      f"_{dat_name.strftime('%S')}.log", "r") as f:
+                cont = f.readline()
+                print(f"{cont} {f.name}")
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
 
 
 if __name__ == "__main__":
