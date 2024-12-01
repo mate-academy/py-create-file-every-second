40df016af0ff688722c0ed0923e24d365093e277
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-12-02 00:37:13 +0100

    adding solution to py-create-file-every-second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def get_name(date: str) -> str:
+    name_first = date[11] + date[12]
+    name_second = date[14] + date[15] if date[14] != 0 else date[15]
+    name_third = date[17] + date[18] if date[17] != 0 else date[18]
+    new_name = str(name_first) + "_" + str(name_second) + "_" + str(name_third)
+    return "app-" + new_name
+
+
+def main() -> None:
+    while True:
+        name = datetime.now()
+        new_name = get_name(str(name)) + ".log"
+        print(name, new_name)
+        with open(new_name, "w") as f:
+            f.write(str(name))
+        sleep(1)
 
 
 if __name__ == "__main__":
