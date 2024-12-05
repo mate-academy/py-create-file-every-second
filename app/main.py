6605f9a81c7f890658caeea78b4a48d569c09d45
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-12-05 17:49:53 +0200

    fixed assertion errors

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from time import sleep
 def main() -> None:
     while True:
         time_now = datetime.now()
-        timestamp = time_now.strftime("%Y-%m-%d %H:%M:%S.%f")
+        timestamp = time_now.strftime("%Y-%m-%d %H:%M:%S")
 
         time_part = f"{time_now.hour}_{time_now.minute}_{time_now.second}"
         file_name = f"app-{time_part}.log"
