5e0e7ee9699b7ef0ac16484488aca357b5929ad2
Dell <gyrkalomas@gmail.com>
2024-12-14 17:27:44 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ from time import sleep
 def main() -> None:
     while True:
         current_data = datetime.now()
-        file_name = f"app-{current_data.hour}_"
-        f"{current_data.minute}_{current_data.second}.log"
+
+        file_name = current_data.strftime("app-%H_%M_%S.log")
+
         timestamp = current_data.strftime("%Y-%m-%d %H:%M:%S")
 
         with open(file_name, "w") as f:
