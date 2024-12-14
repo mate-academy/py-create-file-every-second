7e34ffa892a408c4b906a5c7ad7dede2f7781e2b
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-14 08:57:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,24 +1,20 @@
 from datetime import datetime
-from time import sleep
+import time
 
-try:
-    while True:
-        # Get current timestamp
-        timestamp = datetime.now()
+while True:
+    # Get current timestamp
+    current_time = datetime.now()
 
-        # Format file name
-        file_name = (
-            f"app-{timestamp.hour}_{timestamp.minute}_{timestamp.second}.log"
-        )
+    # Format the filename and the timestamp
+    file_name = current_time.strftime("app-%H_%M_%S.log")
+    timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
 
-        # Write the timestamp to the file
-        with open(file_name, "w") as file:
-            file.write(str(timestamp))
+    # Create and write to the file
+    with open(file_name, "w") as f:
+        f.write(timestamp)
 
-        # Print the output to console
-        print(f"{timestamp} {file_name}")
+    # Print the timestamp and the newly created file name
+    print(f"{timestamp} {file_name}")
 
-        # Wait for 1 second
-        sleep(1)
-except KeyboardInterrupt:
-    print("\nProcess terminated by user.")
+    # Wait for 1 second before the next iteration
+    time.sleep(1)
