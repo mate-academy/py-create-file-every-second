0db9b1e7884726a96ce0028748a58e035f00df95
AntonKrinichnyi <a.krinichnyi@gmail.com>
2024-12-15 11:45:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        currency_date = datetime.now()
+        file_content = str(datetime.now())
+        file_name = currency_date.strftime("app-%H_%M_%S.log")
+        with open(file_name, "w") as file:
+            file.write(file_content)
+        print(f"Create file {file_name} succesfully")
+        sleep(1)
 
 
 if __name__ == "__main__":
