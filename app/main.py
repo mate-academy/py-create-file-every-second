f33e557da2f49a481caca3ed66a3ff252cf0a61c
Bohdan Khudiakov <b.khudiakov@gmail.com>
2024-12-17 19:52:37 +0200

    Solution 5.0

--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ import time
 
 
 def main() -> None:
-    time_format: str = "%H_%M_%S"
+    time_format1: str = "%H_%M_%S"
+    time_format2: str = "%Y-%m-%d %H:%M:%S"
     while True:
         current_time: datetime = datetime.now()
-        file_name: str = f"app-{current_time.strftime(time_format)}.log"
+        file_name: str = f"app-{current_time.strftime(time_format1)}.log"
 
         with open(file_name, "w") as file:
             file.write(current_time.strftime("%Y-%m-%d %H:%M:%S"))
 
-        print(f"{current_time.strftime("%Y-%m-%d %H:%M:%S")} {file_name}")
+        print(f"{current_time.strftime(time_format2)} {file_name}")
 
         time.sleep(1)
 
