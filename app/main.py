6963a97a340f08729383557026915f88cda852bc
Olexander Lypovenko <fantomasdeft@gmail.com>
2024-12-20 00:22:12 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        current_time = datetime.now()
+        file_name = (
+            "app-"
+            f"{current_time.hour}_"
+            f"{current_time.minute}_"
+            f"{current_time.second}.log"
+        )
+        timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(file_name, "w") as file:
+            file.write(timestamp.strip())
+        print(f"{timestamp} {file_name}")
+        sleep(1)
 
 
 if __name__ == "__main__":
