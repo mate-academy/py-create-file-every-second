f6663248d0ad80c8390438d2c62be449afab6a5a
neeeksa <avgust.k31@gmail.com>
2024-12-20 17:31:39 +0200

    all tests have passed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        timing = datetime.now()
+        cleared_time = str(timing).split(" ")
+        timex = (cleared_time[1].split("."))
+        ready_string = f'app-{timex[0].replace(":", "_")}.log'
+        with open(ready_string, "w") as file:
+            file.write(str(timing))
+        print(f"{timing} {ready_string}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
