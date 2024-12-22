afb6b064bd83677d62d9721f16771eb02ac849ad
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-22 12:50:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,13 @@
-from datetime import datetime
 import time
+from datetime import datetime
 
 while True:
-    # Get the current timestamp
-    current_time = datetime.now()
+    now = datetime.now()
+    file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
 
-    # Format the filename based on current time
-    file_name = (f"app-{current_time.hour}_{current_time.minute}_"
-                 f"{current_time.second}.log")
+    with open(file_name, "w") as f:
+        f.write(now.isoformat())
 
-    # Open the file in write mode and write the timestamp
-    with open(file_name, "w") as file:
-        file.write(current_time.isoformat())
+    print(f"{now} {file_name}")
 
-    # Print the timestamp and the file name
-    print(f"{current_time.isoformat()} {file_name}")
-
-    # Wait for 1 second before creating the next file
     time.sleep(1)
