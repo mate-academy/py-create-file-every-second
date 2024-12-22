fb28674c5a48a3491c476cf6debb4abf90bc8470
Dmytro.Poberezhnyi <foxymood@gmail.com>
2024-12-22 16:57:27 +0200

    develop

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        extention = ".log"
+        app = "app-"
+        current_time = datetime.now()
+        with open(f"{app}{current_time.hour}"
+                  f"_{current_time.minute}"
+                  f"_{current_time.second}"
+                  f"{extention}", "w"
+                  ) as f:
+            f.write(str(datetime.now()))
+            print(datetime.now(), f.name)
+            sleep(1)
 
 
 if __name__ == "__main__":
