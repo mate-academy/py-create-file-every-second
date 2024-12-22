46fec177b7ca65612ec6231db776485d2291fdbc
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-22 14:34:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
+# app/main.py
 from datetime import datetime
 from time import sleep
-
 
 def create_file() -> None:
     """
@@ -8,21 +8,19 @@ def create_file() -> None:
     The filename depends on the current time.
     """
     while True:
-        # Get the current time
         now = datetime.now()
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-
-        # Write the timestamp to the file
         with open(file_name, "w") as log_file:
             log_file.write(timestamp)
-
-        # Print information about the created file
         print(f"{timestamp} -> {file_name}")
-
-        # Wait 1 second before creating the next file
         sleep(1)
 
+def main() -> None:
+    """
+    Main function to start the file creation process.
+    """
+    create_file()
 
 if __name__ == "__main__":
-    create_file()
+    main()
