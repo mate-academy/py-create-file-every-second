cb6f1b8fdaf6c9a3f70f1d488dc4b29f01925681
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-22 13:05:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,15 @@ from datetime import datetime
 from time import sleep
 
 
-def create_file() -> None:
+def create_file():
     while True:
         now = datetime.now()
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        with open(file_name, "w") as f:
+        with open(file_name, 'w') as f:
             f.write(timestamp)
         print(f"{timestamp} {file_name}")
         sleep(1)
-
 
 if __name__ == "__main__":
     create_file()
