c7918295443244f55bf434058701f35bdb774c9b
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-22 13:00:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 from time import sleep
-import os
 
-def create_file():
+def create_file() -> None:
     while True:
         now = datetime.now()
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        with open(file_name, 'w') as f:
+        with open(file_name, "w") as f:
             f.write(timestamp)
         print(f"{timestamp} {file_name}")
         sleep(1)
