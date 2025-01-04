1db42e4189720b3d6a06fd068cfc11e5549d81b5
Mazurkav <aleshkamazurkevich@gmail.com>
2025-01-04 16:30:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-from time import sleep
+import time
 
 
 def create_file() -> None:
     while True:
-        now = datetime.now()
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
-        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        current_time = time.localtime()
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S.%f", current_time)
+        file_name = f"app-{current_time.tm_hour}_{current_time.tm_min}_{current_time.tm_sec}.log"
         with open(file_name, "w") as f:
             f.write(timestamp)
         print(f"{timestamp} {file_name}")
-        sleep(1)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
