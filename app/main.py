24ecddcd0ccf6ceb5d035a2664dbf172b008af03
Mazurkav <aleshkamazurkevich@gmail.com>
2025-01-04 16:31:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def create_file() -> None:
     while True:
         current_time = time.localtime()
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S.%f", current_time)
-        file_name = f"app-{current_time.tm_hour}_{current_time.tm_min}_{current_time.tm_sec}.log"
+        file_name = {
+            f"app-{current_time.tm_hour}_{current_time.tm_min}_{current_time.tm_sec}.log"
+        }
         with open(file_name, "w") as f:
             f.write(timestamp)
         print(f"{timestamp} {file_name}")
