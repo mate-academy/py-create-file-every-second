e30472a82c93149991b11dd61225e29b1fca49d7
Vlad Bukin <vbukinnn2009@gmail.com>
2025-02-11 19:42:17 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
+from typing import NoReturn
 
 
-def main():
-    # write your code here
-    pass
+def generate_log_files() -> NoReturn:
+    while True:
+        now = datetime.now()
+
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+
+        with open(filename, "w") as f:
+            f.write(str(now))
+
+        print(f"{now} {filename}")
+
+        sleep(1)
+
+
+def main() -> NoReturn:
+    generate_log_files()
 
 
 if __name__ == "__main__":
