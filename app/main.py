cf83b2c1cc20c5353a3113897b9fa4ad8f044fa8
Marek Smolana <msmolana@gmail.com>
2025-02-18 16:09:47 +0100

    draft

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-import asyncio
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
-async def main() -> None:
+def main() -> None:
     while True:
-        await asyncio.sleep(1)
+        time.sleep(1)
         now = datetime.now()
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(file_name, "w") as output_file:
             output_file.write(f"{now.__str__().replace(':', '_')} {file_name}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
