212800a330fd75c7300dd1cc80021b8628cac1e7
Marek Smolana <msmolana@gmail.com>
2025-02-18 15:34:26 +0100

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
-import os
 import asyncio
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
-async def main():
+
+async def main() -> None:
     while True:
         await asyncio.sleep(1)
         now = datetime.now()
-        file_name =f"app-{now.hour}_{now.minute}_{now.second}.log"
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(file_name, "w") as output_file:
-            output_file.write(now.__str__().replace(":", "_") + f" {file_name}")
+            output_file.write(f"{now.__str__().replace(":", "_")} {file_name}")
             print(f"{now.__str__()} {file_name}")
-    
+
 if __name__ == "__main__":
     asyncio.run(main())
-
