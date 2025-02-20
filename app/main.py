b0a23b54ae72dba4c42e01dcb6ecd4185ac87386
Your Name <stas101203rakitin@gmail.com>
2025-02-20 16:43:48 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    def create_log_files() -> None:
+        while True:
+            timestamp = datetime.now()
+            file_name = (f"app-{timestamp.hour}_{timestamp.minute}"
+                         f"_{timestamp.second}.log")
+            file_content = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+            with open(file_name, "w", encoding="utf-8") as file:
+                file.write(file_content)
+
+            print(f"{file_content} {file_name}")
+            sleep(1)
+
+    create_log_files()
 
 
 if __name__ == "__main__":
