dad0cc8b1b29d8606e6b8d90de61f4d7ff1ce7c9
Maria Hrabovska <mariya76om@gmail.com>
2025-02-26 16:44:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
+def main() -> None:
+    while True:
+        # Отримуємо поточний час
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+
+        # Записуємо таймстемп у файл
+        with open(filename, "w") as file:
+            file.write(timestamp)
+
+        # Виводимо інформацію у консоль
+        print(timestamp, filename)
+
+        # Чекаємо 1 секунду перед наступною ітерацією
+        sleep(1)
     # write your code here
     pass
 
