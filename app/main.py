db449d833491f9799f76819417459eec6c6f7d22
Nikita Tkachenko <7heswatgg@gmail.com>
2025-03-03 21:48:11 +0100

    Solution 2

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ def main() -> None:
     while True:
         now = datetime.now()
         with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "w") as f:
-            f.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}")
+            f.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')}")
 
-        print(f"{now.strftime("%Y-%m-%d %H:%M:%S")}"
+        print(f"{now.strftime('%Y-%m-%d %H:%M:%S')}"
               f" app-{now.hour}_{now.minute}_{now.second}.log")
         sleep(1)
 
