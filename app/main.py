75e9488508ebfe76df19ca576871cdf423de7668
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-03-08 21:57:49 +0200

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -4,22 +4,23 @@ from time import sleep
 
 def main() -> None:
     while True:
+        current_time = (
+            f"{datetime.now().year}-"
+            f"{datetime.now().month}-"
+            f"{datetime.now().day} "
+            f"{datetime.now().hour}:"
+            f"{datetime.now().minute}:"
+            f"{datetime.now().second}"
+        )
         hours = datetime.now().hour
         minutes = datetime.now().minute
         seconds = datetime.now().second
 
         file_name = f"app-{hours}_{minutes}_{seconds}.log"
         with open(file_name, "w") as f:
-            f.write(f"{datetime.now().year}-"
-                    f"{datetime.now().month}-"
-                    f"{datetime.now().day} "
-                    f"{datetime.now().strftime("%H:%M:%S")}")
+            f.write(current_time)
 
-        print(f"{datetime.now().year}-"
-              f"{datetime.now().month}-"
-              f"{datetime.now().day} "
-              f"{datetime.now().strftime("%H:%M:%S")} "
-              f"{file_name}")
+        print(f"{current_time} {file_name}")
         sleep(1)
 
 
