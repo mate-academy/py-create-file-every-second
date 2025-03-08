80065476dc070fbb29d8518b55cba2b13e4e5975
Volodymyr Vitsenko <your@mail.com>
2025-03-08 15:23:12 +0100

    logger

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from time import sleep
 
 def main() -> None:
     while True:
-        file_name = (
-            f"app-{str(datetime.now()).split(" ")[1].replace(
-                ":",
-                "_"
-            )}.log")
+        file_name = f"app-{str(
+            datetime.now()
+        ).split(" ")[1].replace(
+            ":",
+            "_"
+        )}.log"
         with open(file_name, "w") as f:
             f.write(f"{datetime.now()}")
         print(f"{datetime.now()} {file_name}")
