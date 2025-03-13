1e5ba5ef98e216b0d045a0d8401336be715e1081
ExampleE <example@example.example>
2025-03-13 16:15:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,19 +2,19 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
         hours = now.hour
         minutes = now.minute
         seconds = now.second
-        filena = f"app-{hours}_{minutes}_{seconds}.log"
-        file = open(filena, "w+")
+        timefilena = f"app-{hours}_{minutes}_{seconds}.log"
+        timefile = open(timefilena, "w+")
         content = str(now)
         content = content.rsplit(".")[0]
-        file.write(content)
-        file.close()
-        print(content + " " + filena)
+        timefile.write(content)
+        timefile.close()
+        print(content + " " + timefilena)
         sleep(1)
 
 
