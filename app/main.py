a85854f4287796e84792a717073edff603e8aa4a
Dmytro Lisovyi <kenedy11@ukr.net>
2025-03-18 22:31:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
-def main():
+def main() -> str:
+    while True:
+        with open(f"app-{datetime.now().strftime("%H_%M_%S")}.log", "w") as f:
+            f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+              f"app-{datetime.now().strftime("%H_%M_%S")}.log")
+        time.sleep(1)
     # write your code here
     pass
 
