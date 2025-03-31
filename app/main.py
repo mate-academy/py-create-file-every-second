ed94a97b667342de11725b2c4878605cdcf61b12
Kacper Lebocinski <kaclebocinski@gmail.com>
2025-03-31 11:24:06 +0200

    fixed Solution

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from datetime import datetime
 
 def main() -> None:
 
-    now = datetime.now()
-    formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+    while True:
 
-    with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "a") as file:
-        while True:
+        now = datetime.now()
+        formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "a") as file:
             file.write(formatted_now)
             print(f"{formatted_now} {file.name}")
             time.sleep(1)
