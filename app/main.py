2ed807af281d35d3f0d0f604b9899362a2d9e9ae
Kacper Lebocinski <kaclebocinski@gmail.com>
2025-03-31 11:26:59 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def main() -> None:
         now = datetime.now()
         formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
 
-        with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "a") as file:
+        with open(
+                f"app-{now.hour}_{now.minute}_{now.second}.log", "a"
+        ) as file:
             file.write(formatted_now)
             print(f"{formatted_now} {file.name}")
             time.sleep(1)
