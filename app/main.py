b921e2e12bee7f14ed402d2ac8c8b604b5a393b3
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-22 14:00:52 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ from time import sleep
 def main() -> None:
     while True:
         current_time = datetime.now()
-        file_name = f"app-{current_time.hour}-{current_time.minute}-{current_time.second}.log"
+        file_name = (f"app-{current_time.hour}-"
+                     f"{current_time.minute}-"
+                     f"{current_time.second}.log")
         timestamp_content = current_time.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_name, "w") as file:
             file.write(timestamp_content)
