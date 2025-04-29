108f641602d61a07a4393dcc37956f331102642e
Danylo-D87 <quaestor88@gmail.com>
2025-04-29 18:48:44 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+   while True:
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%H_%M_%S")  # змінили формат
+        filename = f"app-{formatted_time}.log"
+        with open(filename, "w") as f:
+            f.write(f"{current_time}")
+        print(f"{current_time} {filename}")
+        sleep(1)
 
 
 if __name__ == "__main__":
