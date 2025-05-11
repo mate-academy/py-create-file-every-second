70618caaff6fc434c107718717d8eae1c5ad09fb
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-11 13:34:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ def main() -> None:
 
     while True:
         time.sleep(1)
-        now = datetime.now()
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-        file_name = f"app-{now.strftime("%H_%M_%S")}.log"
+        now_time = datetime.now()
+        timestamp = now_time.strftime("%Y-%m-%d %H:%M:%S")
+        file_name = f"app-{now_time.strftime("%H_%M_%S")}.log"
         with open(file_name, "a") as file:
             file.write(f"{timestamp}\n")
         print(timestamp, file_name)
