d74cb4ca86e1ed4914c607e19f9fda3f289a38d5
Walery Demidko <walerydz@gmail.com>
2025-05-22 21:29:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from time import sleep
 def main() -> None:
     while True:
         dt_now = datetime.now()
-        log_name = f"app-{dt_now.strftime("%H_%M_%S")}.log"
+        log_name = f'app-{dt_now.strftime("%H_%M_%S")}.log'
         log_data = dt_now.strftime("%Y-%m-%d %H:%M:%S")
 
         with open(log_name, "w") as f:
