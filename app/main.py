317efb3734a219bb262013f86d292a261e8fbfd5
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-25 16:36:54 +0200

    file create data and time

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        current_time = datetime.now()
+        timestamp = (f"{current_time.year}-"
+                     f"{str(current_time.month).zfill(2)}-"
+                     f"{str(current_time.day).zfill(2)}"
+                     f" {str(current_time.hour).zfill(2)}"
+                     f":{str(current_time.minute).zfill(2)}"
+                     f":{str(current_time.second).zfill(2)}")
+        file_name = (f"app-{current_time.hour}_"
+                     f"{current_time.minute}_"
+                     f"{current_time.second}.log")
+        with open(file_name, "w") as f:
+            f.write(timestamp)
+        print(timestamp, file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
