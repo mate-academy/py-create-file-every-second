a3ae7e0a94d31439745f9174b6ea558239adebdd
nkkmkk <nikitmkk@gmail.com>
2025-05-28 15:57:40 +0200

    Fix infinity work

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ from time import sleep
 
 
 def main() -> None:
-    now_time = datetime.now()
-    file_name = (
-        f"app-{now_time.hour}_"
-        f"{now_time.minute}_"
-        f"{now_time.second}.log"
-    )
-    with open(file_name, "w") as file:
-        file.write(str(now_time))
-    print(f"{now_time} {file_name}")
+    while True:
+        now_time = datetime.now()
+        file_name = (
+            f"app-{now_time.hour}_"
+            f"{now_time.minute}_"
+            f"{now_time.second}.log"
+        )
+        with open(file_name, "w") as file:
+            file.write(str(now_time))
+        print(f"{now_time} {file_name}")
+        sleep(1)
 
 
 if __name__ == "__main__":
-    while True:
-        main()
-        sleep(1)
+    main()
