e3a3b920fdd9f4f75e0e694f4eb74703ec26ef72
Ihor Pershin <issue.07.08.09@gmail.com>
2025-05-30 21:29:18 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+import time
+import app.main as app_module
 
 
-def main():
-    # write your code here
-    pass
+date_now = datetime.now()
+
+
+def main() -> None:
+    while True:
+        date_str = app_module.datetime.now()
+        time_file_name = date_str.strftime("app-%H_%M_%S")
+        log_time_file = f"{time_file_name}.log"
+        time_point_str = date_str.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(log_time_file, "w") as f:
+            f.write(f"{time_point_str}")
+
+        print(f"{time_point_str} {log_time_file}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
