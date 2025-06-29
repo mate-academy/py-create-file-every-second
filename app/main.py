21a4533b5fb9f6d3b6b1e8e6480d0e21bfe55b47
Hennadii Mukhin <mukhin_gena@ukr.net>
2025-06-29 19:48:11 +0300

    'Solution-2'

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ def main() -> None:
             year = now.year
             month = now.month
             day = now.day
-            file_name = f"app-{hours}_{minutes}_{seconds}.log"
-            text = (f"{year}-{month}-{day} "
-                    f"{hours}:{minutes}:{seconds}")
+            file_name = f"app-{hours:02d}_{minutes:02d}_{seconds:02d}.log"
+            text = (f"{year}-{month:02d}-{day:02d} "
+                    f"{hours:02d}:{minutes:02d}:{seconds:02d}")
             print(text, file_name)
             with open(file_name, "w") as file:
                 file.write(text)
-                time.sleep(1)
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Process completed")
 
