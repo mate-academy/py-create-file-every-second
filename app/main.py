66d2d80ca3df37bf574e31786008094625e7fb87
Oleksandr <gubar3113@gmail.com>
2025-07-06 20:58:59 +0300

    upd

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ def main() -> None:
         file_and_content = (f"app-{int(current_time.hour)}_"
                             f"{int(current_time.minute)}_"
                             f"{int(current_time.second)}"
-                            )
+                            f".log")
         times_date = current_time.strftime("%Y-%m-%d %H:%M:%S")
-        with open(f"{file_and_content}.log", "w") as f:
+        with open(f"{file_and_content}", "w") as f:
             f.write(times_date)
         print(f"{times_date} {file_and_content}")
         sleep(1)
