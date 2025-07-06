a6a171960bffe2e3f2aa71bd0c6cf8c324206303
godfri <godfri@kali>
2025-07-06 22:13:15 +0300

    compl

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
 def main():
     # write your code here
-    pass
+    while True:
+        current_time = datetime.now()
+        timestamp_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        filename = f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log"
+
+        with open(filename, "w") as f:
+            f.write(timestamp_str)
+
+        print(f"{timestamp_str} {filename}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
