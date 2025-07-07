6d7eb969b9fff176b698380dfabe28ba5c56f3b9
Vlasov Misha <vlasovmisha.060@gmail.com>
2025-07-07 16:35:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,24 @@
 from datetime import datetime
 from time import sleep
 
-def main():
+
+def main() -> None:
+
     while True:
         now = datetime.now()
 
-        filename = f"app-{now.strftime('%H_%M_%S')}.log"
+        filename = f"app-{now.strftime("%H_%M_%S")}.log"
 
         # Запис у файл без переносу рядка
         with open(filename, "w") as file:
-            file.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')}")
+            file.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}")
 
         # Вивід у консоль без \n
-        print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {filename}", end="")
+        print(f"{now.strftime("%Y-%m-%d %H:%M:%S")} {filename}", end="")
 
         sleep(1)
         print()  # Ручний перехід на новий рядок для зручності
+
 
 if __name__ == "__main__":
     main()
