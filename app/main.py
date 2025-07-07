bfd071da872b27f5a3ec021a1793e541e787d2a7
Vlasov Misha <vlasovmisha.060@gmail.com>
2025-07-07 16:48:50 +0300

    Solutionn

--- app/main.py
+++ app/main.py
@@ -9,15 +9,15 @@ def main() -> None:
 
         filename = f"app-{now.strftime("%H_%M_%S")}.log"
 
-        # Запис у файл без переносу рядка
+
         with open(filename, "w") as file:
             file.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}")
 
-        # Вивід у консоль без \n
+
         print(f"{now.strftime("%Y-%m-%d %H:%M:%S")} {filename}", end="")
 
         sleep(1)
-        print()  # Ручний перехід на новий рядок для зручності
+        print()
 
 
 if __name__ == "__main__":
