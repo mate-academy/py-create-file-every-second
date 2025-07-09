a9c19c6342fb6c6374945b1c87685126a4540afd
Vadym Vizirov <vadimplus57@gmail.com>
2025-07-09 13:32:56 +0300

    create file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        time_now = datetime.now()
+        format_name_file = (f"app-{time_now.hour}_{time_now.minute}"
+                            f"_{time_now.second}.log")
+        with open(format_name_file, "w") as write_file:
+            print_format = time_now.strftime("%Y-%m-%d %H:%M:%S")
+            write_file.write(f"{print_format}")
+            print(f"{print_format} {format_name_file}")
+            time.sleep(1)
 
 
 if __name__ == "__main__":
