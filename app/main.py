2de64d8a41fba3cc65d8d99eddadc06ec1b9cf62
yarik <super.racing@ukr.net>
2025-07-09 22:40:38 +0300

    changed stylization

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
@@ -10,6 +10,7 @@ def main():
             f.write(f"{now}")
             print(f.read())
             sleep(1)
+
 
 if __name__ == "__main__":
     main()
