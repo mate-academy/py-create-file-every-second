7ea815e6a5021863ddbf6217953194a648b23fd5
Mykola Totsevych <totsevich02@gmail.com>
2025-07-09 15:39:02 +0300

    Fixed flake8 error x2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main() -> None:
                 f"{datetime.now().second}.log", "w"
         ) as log_file:
             log_file.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-        print(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} "
+        print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} '
               f"app-{datetime.now().hour}_{datetime.now().minute}_"
               f"{datetime.now().second}.log")
         sleep(1)
