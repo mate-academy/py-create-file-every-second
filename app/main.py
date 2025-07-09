8107afc53fd76d7af8203cdbb06b4942061ff943
yarik <super.racing@ukr.net>
2025-07-09 22:52:27 +0300

    changed mode

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main() -> None:
     while True:
         now = datetime.now()
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        with open(file_name, "r+") as f:
+        with open(file_name, "w+") as f:
             f.write(f"{now}")
             print(f.read())
             sleep(1)
