8fe811c2e938c96237e284bd31696be38a0a4931
Vlasov Misha <vlasovmisha.060@gmail.com>
2025-07-09 22:34:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,8 @@ def main() -> None:
 
         filename = f"app-{now.strftime("%H_%M_%S")}.log"
 
-
         with open(filename, "w") as file:
             file.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}")
-
 
         print(f"{now.strftime("%Y-%m-%d %H:%M:%S")} {filename}", end="")
 
