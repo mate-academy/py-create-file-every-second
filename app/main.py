362e54f849213c8afd647ad393bb69afc519b261
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-13 13:42:01 +0700

    It turned out that usage of .now() and .strftime() methods is not allowed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+        hrs, mins, secs = now.split()[1].split(":")
+        secs = secs if secs[0] != "0" else secs[1]
+        with open(f"app-{hrs}_{mins}_{secs}.log", "w") as f:
+            f.write(now)
+        sleep(1)
 
 
 if __name__ == "__main__":
