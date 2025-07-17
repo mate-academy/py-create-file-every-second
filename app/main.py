ec32e743d5369428e7fcf0f257066c142cb460c2
mr_eugene <roberttimethone@gmail.com>
2025-07-17 15:04:37 +0200

    sol1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
+from datetime import datetime
 
+def main() -> None:
+    while True:
+        date_now = datetime.now().strftime("%Y-%m-%d")
+        time_now = datetime.now().strftime("%H:%M:%S")
+        filename = f"app-{time_now.replace(':', '_')}.log"
 
-def main():
-    # write your code here
-    pass
+        with open(filename, "w") as f:
+            f.write(f"{date_now} {time_now}")
+        print(f"{date_now} {time_now} {filename}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
