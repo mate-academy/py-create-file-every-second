0682df9afc517846e48310247abc51f431ac10c7
Vlasov Misha <vlasovmisha.060@gmail.com>
2025-07-18 21:06:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ def main() -> None:
     while True:
         now = datetime.now()
 
-        filename = (f"app-{now.strftime("%H_%M_%S")}.log")
+        filename = f"app-{now.strftime('%H_%M_%S')}.log"
 
         with open(filename, "w") as file:
-            file.write(f"{now.strftime("%Y-%m-%d %H:%M:%S")}")
+            file.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')}")
 
-        print(f"{now.strftime("%Y-%m-%d %H:%M:%S")} {filename}", end="")
+        print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {filename}", end="")
 
         sleep(1)
         print()
