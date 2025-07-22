c07532153fa9eb03218767c2667231a8d80241a8
Dasha <dariia.leshchenko@gmail.com>
2025-07-22 23:34:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main() -> None:
         filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(filename, "w") as f:
             f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
-        print(f"{now.strftime("%Y-%m-%d %H:%M:%S")} {filename}")
+        print(f'{now.strftime("%Y-%m-%d %H:%M:%S")} {filename}')
         sleep(1)
 
 
