716908ebcd017310bacc1c708c6f5854e3b00a05
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2025-07-28 17:08:23 +0300

    Fix flake

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main() -> None:
     while True:
         current_date = datetime.now()
         current_timestamp = current_date.strftime("%Y-%m-%d %H:%M:%S")
-        file_name = f"app-{current_date.strftime("%H_%M_%S")}.log"
+        file_name = f"app-{current_date.strftime('%H_%M_%S')}.log"
 
         with open(file_name, "a") as file:
             file.write(current_timestamp)
