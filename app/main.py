97c86718ced6df050491c5e43ef62bbbdd3e5caf
Volodymyr Syvak <volodimirsivak388@gmail.com>
2025-08-05 02:41:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,13 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        time = datetime.now().split(":")
+        file_name = f"app-{time[0][-2,-1]}_{time[1]}_{time[2]}.log"
+        with open(f"file_name", "w") as f:
+            f.write(datetime.now())
+            print(datetime.now(), file_name)
+
 
 
 if __name__ == "__main__":
