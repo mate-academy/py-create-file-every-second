0aaf31a14011996ce72ce856ee92730db91cfe86
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-08-13 17:54:29 -0300

    New Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> str:
     while True:
         now = datetime.now()
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
