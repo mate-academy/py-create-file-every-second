bacd6568708a8e36f37326142892e4ae0341a5df
Vladislav Lykhoiedenko <v.lykhoiedenko@gmail.com>
2025-08-16 12:09:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main() -> None:
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
         print(f"{text} {file_name}", flush=True)
         with open(file_name, "w") as file:
-            file.write(text)#
+            file.write(text)   #
         time.sleep(1)
 
 
