90968ab404a79ab9e9d3b17d9a7d2e19e3c2a161
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-08-25 19:12:39 +0300

    'super_new_Solution'

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ def main() -> None:
 
     while True:
         time = datetime.now()
-        with open(f"app-{time.strftime(format="%H_%M_%S")}.log", "w") as logfile:
-            logfile.write(f"{time.date()} {time.strftime(format="%H:%M:%S")}")
-        print(f"{time.date()} {time.strftime(format="%H:%M:%S")} "
-              f"{f"app-{time.strftime(format="%H_%M_%S")}.log"}")
+        with open(f"app-{time.strftime('%H_%M_%S')}.log", "w") as logfile:
+            logfile.write(f"{time.date()} {time.strftime('%H:%M:%S')}")
+        print(f"{time.date()} {time.strftime('%H:%M:%S')} "
+              f"{f"app-{time.strftime('%H_%M_%S')}.log"}")
         sleep(1)
 
 
