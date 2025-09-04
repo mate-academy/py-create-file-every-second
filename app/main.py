518b41a38fad23e02784b2fb3ab5066cfad7b7ad
Sabina Gamidova <sabina.gamidova.dev@gmail.com>
2025-09-04 15:12:01 +0300

    Solution: resolved conflicts

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from time import sleep
 def main() -> None:
     while True:
         now_time = datetime.now()
-        file_name = f"app-{now_time.strftime("%H_%M_%S")}.log"
+        file_name = "app-" + now_time.strftime("%H_%M_%S") + ".log"
         file_content = now_time.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_name, "w") as file:
             file.write(file_content)
