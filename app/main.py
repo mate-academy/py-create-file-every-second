2c91d033e8fb25b0ad6099e89be6ec55b69cae43
Yurii <yurij.kenu@gmail.com>
2025-09-06 16:42:17 +0200

    create-file-every-sec: implemented

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
+
+DELAY = 1
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+
+        with open(file_name, "a") as f:
+            f.write(str(now))
+
+        print(now, file_name)
+        sleep(DELAY)
 
 
 if __name__ == "__main__":
