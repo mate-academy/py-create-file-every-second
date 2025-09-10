6a79ec5a617f9a7ec6fdf0e99ae08a34e1bc0b0d
Gena <gena.anzin@ukr.net>
2025-09-10 16:29:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
+
 def main() -> None:
     while True:
         time = datetime.now()
-        with open(f"app-{time.hour}_{time.minute}_{time.second}.log", "w") as f:
+        with open(
+                f"app-{time.hour}_{time.minute}_{time.second}.log", "w") as f:
             f.write(str(datetime.now()))
-            print(f"{time} app-{time.hour}_{time.minute}_{time.second}.log")
+            print(
+                f"{time} app-{time.hour}_{time.minute}_{time.second}.log")
         sleep(1)
 
 
