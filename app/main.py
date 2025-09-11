41f6379983aadae2a28ba4535a3d084ec61226b2
Lauênia <laueniafr@outlook.com>
2025-09-11 12:33:46 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
+while True:
+    now = datetime.now()
+    timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    # é um metodo que transforma esse objeto datetime em uma string formatada.
+    file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
 
-def main():
-    # write your code here
-    pass
+    with open(file_name, "w") as f:
+        f.write(timestamp_str)
+
+    print(f"{timestamp_str} {file_name}")
+
+    sleep(1)
 
 
 if __name__ == "__main__":
