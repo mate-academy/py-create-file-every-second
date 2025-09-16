814f88f0fccf9ee713e204685b1459137a628944
0lex1y <kotov.oleksiy@icloud.com>
2025-09-17 02:35:19 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
+from datetime import datetime
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        file_name = now.strftime("app-%H_%M_%S.log")
+        content = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open(file_name, "w") as file:
+            file.write(content)
+        print(content, file_name)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
