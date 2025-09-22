8950b57570f15d2aafd8a692d9dac318a43aa1b9
Vasyl Cherkes <cherkesva@gmail.com>
2025-09-22 23:58:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ def main() -> None:
     while True:
         now = datetime.now()
         hour, minute, second = now.hour, now.minute, now.second
-        file_name = f"app-{hour}_{minute}_{second}.log"
+        cur_time = time(hour, minute, second)
+        file_name =\
+            f"app-{cur_time.hour}_{cur_time.minute}_{cur_time.second}.log"
         now_time = now.strftime("%Y-%m-%d %H:%M:%S")
         with open(file_name, "w") as f:
             f.write(now_time)
