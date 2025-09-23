0a792089b74fc60c0e7cdaf1308836f3fd2c00de
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-23 14:27:55 +0300

    fix a problem9

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import time
 
-
 def main() -> None:
     try:
         while True:
@@ -12,14 +11,8 @@ def main() -> None:
                 f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
 
             print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {filename}")
-
-            # Використовуємо sleep лише якщо datetime не мокований (тест перевизначає datetime)
-            if hasattr(now, "real"):
-                time.sleep(1)
+            time.sleep(1)  # Просто sleep(1) у звичайному режимі
 
     except KeyboardInterrupt:
+        print('Stopped!')
         pass
-
-
-if __name__ == "__main__":
-    main()
