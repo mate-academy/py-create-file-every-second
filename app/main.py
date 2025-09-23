ef2d88a27f2794ddd8d5aad6273ef88f5bdfd222
Ілля Витовтов <izbastenovarsen@gmail.com>
2025-09-23 14:35:23 +0300

    fix a problem11

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
 from datetime import datetime
 import time
 
-
 def main() -> None:
-    try:
-        while True:
-            now = datetime.now()
-            filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+    while True:
+        now = datetime.now()
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
 
-            with open(filename, "w") as f:
-                f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+        with open(filename, "w") as f:
+            f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
 
-            print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {filename}")
-            time.sleep(1)  # Просто sleep(1) у звичайному режимі
-    except KeyboardInterrupt:
-        print("Stopped!")
-        pass
+        print(f"{now.strftime('%Y-%m-%d %H:%M:%S')} {filename}")
+        time.sleep(1)
