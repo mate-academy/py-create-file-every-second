cc4a7c45e1c1010ef9716cf67a694b8d1e772fdd
Vladislav <grechkosij.vlados@gmail.com>
2025-09-30 16:32:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        formated = now.strftime("app-%H_%M_%S.log")
+        line = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open(formated, "w") as obj_file:
+            obj_file.write(line)
+        print(line, formated)
+        sleep(1)
 
 
 if __name__ == "__main__":
