44fe0254f89df68d1039c8994e93888b0ed98abe
Jeuxu <iliacdmn@gmail.com>
2025-10-07 22:39:58 +0300

    remove f-string

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main() -> None:
         file_name = "app-" + datetime.now().strftime("%H_%M_%S") + ".log"
         with open(file_name, "w") as f:
             f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        print(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {file_name}")
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + file_name)
         sleep(1)
 
 
