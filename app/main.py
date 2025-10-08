99590effeedd23d1276f9edb7abfcc741dcda89a
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-08 12:22:23 +0200

    first commit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime# DO NOT CHANGE THIS IMPORT
+import time
 
 
 def main():
-    # write your code here
-    pass
-
-
+    while True:
+        now = datetime.now()
+        print(now)
+        time.sleep(1)
 if __name__ == "__main__":
     main()
