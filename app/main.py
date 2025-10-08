e12ba218fd3090950db1cdb84da887b279e86e0b
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-08 12:46:07 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
-from datetime import datetime# DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+
+
 import time
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
-        print(now)
+        date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        log_file_name = "app-" + now.strftime("%H_%M_%S") + ".log"
+
+        try:
+            with open(log_file_name, "w") as file:
+                file.write(date_time)
+        except Exception as e:
+            print(e)
+        else:
+            print(f"{date_time} {log_file_name}")
+
         time.sleep(1)
+
+
 if __name__ == "__main__":
     main()
