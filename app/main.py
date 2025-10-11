1145f89b9a9cba0126a57ecd687f7cfcc2b0a95f
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-11 17:39:55 +0200

    main 6

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main() -> None:
         now = datetime.now()
         content = now.strftime("%Y-%m-%d %H:%M:%S")
         filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        with open(filename) as file:
+        with open(filename, "w") as file:
             file.write(f"{content}")
         print(f"{content} {filename}")
         sleep(1)
