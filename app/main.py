b9f97f4ccce0c0799e39136ebdad061a072a7883
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-11 17:36:44 +0200

    main 5

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from time import sleep
 
 def main() -> None:
     while True:
-        time = datetime.now()
-        content = time.strftime("%Y-%m-%d %H:%M:%S")
-        with open(
-                f"app-{time.hour}_{time.minute}_{time.second}.log", "w"
-        ) as file:
+        now = datetime.now()
+        content = now.strftime("%Y-%m-%d %H:%M:%S")
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        with open(filename) as file:
             file.write(f"{content}")
+        print(f"{content} {filename}")
         sleep(1)
 
 
