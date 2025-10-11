8164962cbd6e6f82f875b0d7c0569e0a8b3e58ff
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-11 18:15:25 +0200

    main 8

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from time import sleep
 
 def main() -> None:
     while True:
-        sleep(1)
         now = datetime.now()
-        content = now.strftime("%Y-%m-%d %H:%M:%S")
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
         filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
-        with open(filename, "w") as file:
-            file.write(f"{content}")
-        print(f"{content} {filename}")
+        with open(filename, "w") as f:
+            f.write(timestamp)
+        print(f"{timestamp} {filename}")
+        sleep(1)
 
 
 if __name__ == "__main__":
