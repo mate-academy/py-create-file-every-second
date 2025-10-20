beffe4b12a0a15760ead6bc36716667df258ae75
Luan <luan.valentino78@gmail.com>
2025-10-20 20:09:04 -0300

    Commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 import time
 
+
 def main() -> None:
     while True:
         current_time = datetime.now()
 
-        file_name = f"app-{current_time.hour}_{current_time.minute}_{current_time.second}.log"
+        file_name = (f"app-{current_time.hour}_"
+                     f"{current_time.minute}_"
+                     f"{current_time.second}.log")
         content = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
         with open(file_name, "w") as file:
