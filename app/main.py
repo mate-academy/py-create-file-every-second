00b4d14c31fdecbd461aab6cdb0900d5ce5b875a
Tania Kolesnik <kolesnik@gmail.com>
2025-10-21 21:34:28 +0100

    add solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
+import time
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        curr_time = datetime.now()
+        filename = f'app-{curr_time.strftime("%H_%M_%S")}.log'
+        log_time = curr_time.strftime("%Y-%m-%d %H:%M:%S")
+        write_log(filename, log_time)
+        time.sleep(1)
+
+
+def write_log(filename: str, log_time: str) -> None:
+    with open(filename, "a") as f:
+        f.write(log_time)
+    f.close()
+    print(log_time, filename)
 
 
 if __name__ == "__main__":
