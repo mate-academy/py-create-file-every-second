9cd9d10cb6a273aca6ab77ad8554245aa932c96b
Anton Tsikhanovich <alluspeh@gmail.com>
2025-10-25 00:10:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main() -> None:
     while True:
         day_to_name = datetime.now().strftime("%H_%M_%S")
         name = f"app-{day_to_name}.log"
-        to_file_time = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}"
+        to_file_time = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         with open(name, "w", encoding="utf-8") as f:
             f.write(f"{to_file_time}")
 
