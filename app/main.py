7b5b6a008cf7198e0f93c0cd1480ac4969e462d4
Serhii Markusyk <serega.markusik@gmail.com>
2025-10-26 11:29:54 +0100

    py-create-file-every-second2

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from datetime import datetime
 from time import sleep
 
 
-def main():
+def main() -> None:
 
     while True:
 
         time_now = datetime.now()
 
-        file_name = f"app-{time_now.hour}_{time_now.minute}_{time_now.second}.log"
+        file_name = (f"app-{time_now.hour}_"
+                     f"{time_now.minute}_{time_now.second}.log")
 
         timestamp = time_now.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -18,6 +19,7 @@ def main():
         print(f"{timestamp} {file_name}")
 
         sleep(1)
+
 
 if __name__ == "__main__":
     main()
