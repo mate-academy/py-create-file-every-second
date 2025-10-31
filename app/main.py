e515d9607a7d1ed7fe92e674b1d31c9d0a4a048c
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-10-31 22:49:48 +0000

    program that creates a new file every second and prints its timestamp inside.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+import time
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now_time = datetime.now()
+        timestamp = now_time.strftime("%Y-%m-%d %H:%M:%S")
+        name = "app-" + now_time.strftime("%H_%M_%S") + ".log"
+        with open(name, "w") as file:
+            file.write(timestamp)
+        print(f"{timestamp} {name}")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
