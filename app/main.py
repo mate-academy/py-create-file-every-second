03780053c3385c5db42a1b751b231bb7dd541024
Zanko Yaroslav <arikzanko9@gmail.com>
2025-11-02 09:22:24 +0200

    ms

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import time
 def main() -> None:
     while True:
         now = datetime.now().replace(microsecond=0)
-        file_path = f"app-{now.hour}-{now.minute}_{now.second}.log"
+        file_path = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(file_path, "w") as f:
             f.write(f"{now}")
         print(str(now), file_path)
