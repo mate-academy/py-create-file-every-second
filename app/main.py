1924635fa1a5e3bd1a491f8c6d42f41e6a710f28
Oleksandr <15bizon15@gmail.com>
2025-11-07 15:57:55 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,27 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
+class KeyboardInterrupt(Exception):
     pass
+
+
+def main() -> None:
+    try:
+        while True:
+            now = datetime.now()
+            file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+            timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+            with open(file_name, "w") as f:
+                f.write(timestamp)
+
+            print(f"{timestamp} {file_name}")
+
+            sleep(1)
+
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == "__main__":
