7ed850c01d71e348a21baeed45c6da187433e1f6
Taras Ivashchuk <tvishy@disroot.org>
2025-11-08 19:19:45 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        time_now = datetime.now()
+        hours = time_now.hour
+        minutes = time_now.minute
+        seconds = time_now.second
+        date = time_now.date()
+        filename = f"app-{hours}_{minutes}_{seconds}.log"
+        timestamp = f"{date} {hours}" + ":" + f"{minutes}" + ":" + f"{seconds}"
+        with open(filename, "w") as f:
+            f.write(timestamp)
+            print(f"{timestamp} {filename}")
+        sleep(1)
 
 
 if __name__ == "__main__":
