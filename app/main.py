bbc004035dcb68a09b569f77a9b28f9a6acb6642
Oleg <oleggavelya@gmail.com>
2025-11-08 13:33:41 +0200

    change formating date

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ from time import sleep
 def main() -> None:
     while True:
         current_time = datetime.now()
-        file_name = f"app-{current_time.strftime("%H_%M_%S")}.log"
-        sleep(1)
+        file_name = f"app-{current_time.strftime('%H_%M_%S')}.log"
         with open(file_name, "w") as w:
-            w.write(current_time.strftime("%Y-%m-%d %H:%M:%S"))
+            w.write(current_time.strftime('%Y-%m-%d %H:%M:%S'))
         with open(file_name, "r") as r:
             print(r.readline(), file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
