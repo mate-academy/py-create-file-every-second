98c2aeab77c7ece47bd6bdd9c70b3b9fc54f1b46
Oleg <oleggavelya@gmail.com>
2025-11-08 13:52:17 +0200

    add var format_hms

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ from time import sleep
 def main() -> None:
     while True:
         current_time = datetime.now()
-        file_name = f"app-{current_time.strftime('%H_%M_%S')}.log"
+        format_hms = current_time.strftime("%H_%M_%S")
+        file_name = f"app-{format_hms}.log"
         with open(file_name, "w") as w:
-            w.write(current_time.strftime('%Y-%m-%d %H:%M:%S'))
+            w.write(current_time.strftime("%Y-%m-%d %H:%M:%S"))
         with open(file_name, "r") as r:
             print(r.readline(), file_name)
         sleep(1)
