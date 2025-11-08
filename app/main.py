d73df22282fbbc1d41bacf4524a44c5ebe20f2b5
Andrii Chychkan <mywork1728@gmail>
2025-11-08 19:23:53 +0200

    finished

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    try:
+        while True:
+            current_time_stamp = datetime.now()
+
+            name = (f"app-{current_time_stamp.hour}_"
+                    f"{current_time_stamp.minute}_"
+                    f"{current_time_stamp.second}.log")
+
+            with open(name, "w") as file:
+                file.write(current_time_stamp.strftime("%Y-%m-%d %H:%M:%S"))
+            print(current_time_stamp, name)
+            sleep(1)
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
 
 
 if __name__ == "__main__":
