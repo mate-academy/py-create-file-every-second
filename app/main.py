b6afefd04ea481a1736d7f8fc1ef65b42fadfa5a
Your Name <anastasiacibart8@gmail.com>
2025-11-24 11:30:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        hours, minutes, seconds = now.hour, now.minute, now.second
+        name = f"app-{hours}_{minutes}_{seconds}.log"
+        print(now.strftime("%Y-%m-%d %H:%M:%S"), name)
+        with open(name, "w") as f:
+            # 2020-01-01 14:10:07
+            f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+        sleep(1)
 
 
 if __name__ == "__main__":
