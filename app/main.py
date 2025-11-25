8b433deabaf63bd365d13690e73a3957f207057e
tomech <109527747+tomech4@users.noreply.github.com>
2025-11-25 21:56:33 +0100

    add function return annotation

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 import time
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
         with open(f"app-{now.hour}_{now.minute}_{now.second}.log", "w") as f:
