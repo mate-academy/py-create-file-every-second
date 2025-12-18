827d63488af1fcf4d1e6cea940efc21e914bf052
Darmi555 <mateusz.rudnicki05@wp.pl>
2025-12-18 05:42:36 +0100

    bomba

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        name = datetime.now()
+        log = name.strftime("app-%H_%M_%S.log")
+        with open(log, "w") as f:
+            time = name.strftime("%Y-%m-%d %H:%M:%S")
+            f.write(str(time))
+        print(time + " " + log)
+        sleep(1)
 
 
 if __name__ == "__main__":
