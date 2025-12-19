bfb2a2f0fc153e943e7da4e84f6f468369fde46d
Hideous <hideous198781@gmail.com>
2025-12-19 14:40:23 +0200

    another solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
+
         file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
 
