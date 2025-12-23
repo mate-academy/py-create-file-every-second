662cfef460cf639ab6f8478ea37aa57f52ac9640
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2025-12-23 19:33:16 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,15 @@
 from datetime import datetime
 from time import sleep
 
-
 def main():
     while True:
         now = datetime.now()
         filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
         content = now.strftime("%Y-%m-%d %H:%M:%S")
-
         with open(filename, "w") as f:
             f.write(content)
-
         print(f"{content} {filename}")
         sleep(1)
-
 
 if __name__ == "__main__":
     main()
