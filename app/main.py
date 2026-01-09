725848eae4f20b0ee5083da5c4831f099a7e5f52
Johan Rangel <johan.apr99@gmail.com>
2026-01-09 16:23:33 +0000

    test

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ import time
 def main() -> None:
     while True:
         get_time = datetime.now()
-        with open(f"app-{get_time.hour}_{get_time.minute}_{get_time.second}.log",
-                   "w") as outfile:
+        file_name = f"app-{get_time.hour}_{get_time.minute}_{get_time.second}.log"
+        with open(file_name, "w") as outfile:
             outfile.write(str(datetime.now()))
-            print(f"{datetime.now()} app-{get_time.hour}_{get_time.minute}_{get_time.second}.log")
+            print(f"{datetime.now()} {file_name}")
         time.sleep(1)
 
 
