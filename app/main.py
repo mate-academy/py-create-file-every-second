461412309f8cedb28bda9e5e86d912fcd51aa3a7
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2026-01-10 18:23:39 +0200

    f

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+import time
 
 
-def main():
-    # write your code here
+def main() -> None:
+    def callback() -> None:
+        time_now = datetime.now()
+        name = f"app-{time_now.strftime('%H_%M_%S')}.log"
+        data = f"{time_now}"
+        print(f"{data} {name}")
+
+        with open(name, "a") as f:
+            f.write(data)
+
+    while True:
+        callback()
+        time.sleep(1)
     pass
 
 
