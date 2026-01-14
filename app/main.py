0833ddca40172fee80937dc11b1507573be08709
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-14 18:01:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
+import time
 
-def main():
-    # write your code here
-    pass
 
+def main() -> None:
+    while True:
+        time_now = datetime.now()
+        timestamp = time_now.strftime('%Y-%m-%d %H:%M:%S')
+        hours = time_now.strftime('%H')
+        minutes = time_now.strftime('%M')
+        seconds = time_now.strftime('%S')
+        file_name_format = f"app-{hours}_{minutes}_{seconds}.log"
+        with open(file_name_format, "w") as file_every_second:
+            file_every_second.write(f"{timestamp}")
+            print(f"{timestamp} {file_name_format}")
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
