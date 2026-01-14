5f9223296101e289f26dcdaf848eb4d643118c7d
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-14 18:04:34 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -6,15 +6,16 @@ import time
 def main() -> None:
     while True:
         time_now = datetime.now()
-        timestamp = time_now.strftime('%Y-%m-%d %H:%M:%S')
-        hours = time_now.strftime('%H')
-        minutes = time_now.strftime('%M')
-        seconds = time_now.strftime('%S')
+        timestamp = time_now.strftime("%Y-%m-%d %H:%M:%S")
+        hours = time_now.strftime("%H")
+        minutes = time_now.strftime("%M")
+        seconds = time_now.strftime("%S")
         file_name_format = f"app-{hours}_{minutes}_{seconds}.log"
         with open(file_name_format, "w") as file_every_second:
             file_every_second.write(f"{timestamp}")
             print(f"{timestamp} {file_name_format}")
         time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
