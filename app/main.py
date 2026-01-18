aeea69284e78603e33aa19aac38e5eb2e2fbb50e
Dmytro <ys33ys33ys55@gmail.com>
2026-01-18 19:29:32 +0100

    Solution2

--- app/main.py
+++ app/main.py
@@ -4,12 +4,16 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 def main() -> None:
     while True:
-        current_time = str(datetime.now()).split()[1].split(".")[0].split(":")
+        current_datetime = str(datetime.now())
+        current_time = current_datetime.split()[1].split(".")[0].split(":")
         hours = current_time[0]
         minutes = current_time[1]
         seconds = current_time[2]
-        with open(f"app/app-{hours}_{minutes}_{seconds}.log", "w"):
-            time.sleep(1)
+        name = f"app-{hours}_{minutes}_{seconds}.log"
+        with open(name, "w") as file:
+            file.write(current_datetime)
+        print(str(datetime.now()), name)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
