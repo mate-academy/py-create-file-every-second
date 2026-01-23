68eb2c4e342b15638dcd51921201a56c3f1c756c
titoiriepal <titoiriepal@hotmail.com>
2026-01-23 13:51:23 +0100

    Files upload in current working directory, not in Script directory

--- app/main.py
+++ app/main.py
@@ -6,18 +6,15 @@ BASE_DIR = Path(__file__).resolve().parent
 
 
 def main() -> None:
-    try:
-        while True:
-            c_date = datetime.now()
-            file_route = f" app-{c_date.hour}-{c_date.minute}-"
-            file_route += f"{c_date.second}.log"
-            print(c_date.strftime("%Y-%m-%d %H:%M:%S") + file_route)
-            write_file = open(BASE_DIR / file_route, "w")
-            write_file.write(c_date.strftime("%Y-%m-%d %H:%M:%S"))
-            write_file.close()
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Programa finalizado")
+    while True:
+        c_date = datetime.now()
+        file_route = f"app-{c_date.hour}_{c_date.minute}_"
+        file_route += f"{c_date.second}.log"
+        print(c_date.strftime("%Y-%m-%d %H:%M:%S") + " " + file_route)
+        write_file = open(file_route, "w")
+        write_file.write(c_date.strftime("%Y-%m-%d %H:%M:%S"))
+        write_file.close()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
