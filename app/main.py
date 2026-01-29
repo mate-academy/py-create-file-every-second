d8c879c1dd0f79833abe9db4e2266a13de4346f3
StychukMaria <stychuk.maria@gmail.com>
2026-01-29 13:13:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    try:
+        while True:
+            now = datetime.now()
+            file_write = open(
+                f"app-{now.hour}_{now.minute}_{now.second}.log", "w"
+            )
+            file_write.write(str(now.strftime("%Y-%m-%d %H:%M:%S")))
+            print(
+                f"{now.strftime('%Y-%m-%d %H:%M:%S')} "
+                f"app-{now.hour}_{now.minute}_{now.second}.log"
+            )
+            file_write.close()
+            sleep(1)
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
 
 
 if __name__ == "__main__":
