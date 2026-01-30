20cdc0f3c2d205254158bfe775d21b8d6a1b3a84
Seroga <ilchuk.cergey@gmail.com>
2026-01-30 18:39:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
-def main():
+
+def main() -> None:
     while True:
         now = datetime.now()
         hours, minutes, seconds = now.hour, now.minute, now.second
