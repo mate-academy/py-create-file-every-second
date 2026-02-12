152f22335c6717ce26cc5a40f03cb3ca21dc38d1
ladoc <ladochadominik@gmail.com>
2026-02-12 08:20:35 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def get_filename(now: datetime) -> str:
+    return now.strftime("app-%H_%M_%S.log")
+
+
+def get_timestamp(now: datetime) -> str:
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def main() -> None:
+    while True:
+        now = datetime.now()
+        filename = get_filename(now)
+        content = get_timestamp(now)
+
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(content)
+
+        print(f"{content} {filename}")
+        sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nProcess terminated by user. Stopped.")
