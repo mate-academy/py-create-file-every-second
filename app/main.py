d9698faaf361b6bf10932a0b0194c96ba09d2df3
Illia Savinov <dresar2404@gmail.com>
2026-02-12 15:25:20 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        this_exact_moment = datetime.now()
+        new_file_name = (f"app-{this_exact_moment.hour}_"
+                         f"{this_exact_moment.minute}_"
+                         f"{this_exact_moment.second}.log")
+        with open(new_file_name, "a") as f:
+            f.write(f"{this_exact_moment}")
+        print(this_exact_moment, new_file_name)
+        sleep(1)
 
 
 if __name__ == "__main__":
