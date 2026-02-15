65cfa509e4962372bf3acc083cea540db99c675c
Kostiantyn <kostyapastukh@gmail.com>
2026-02-16 01:11:21 +0200

    create file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        time_now = (f""
+                    f"app-{str(
+                        datetime.now().time()
+                    )[:8].replace(':', '_')}.log"
+                    f"")
+        with open(time_now, "w") as file:
+            file.write(f"{str(datetime.now())}")
+            print(f"{str(datetime.now())} {time_now}")
+
+        sleep(1)
 
 
 if __name__ == "__main__":
