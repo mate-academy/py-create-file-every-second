3cd44d7d334282f9651e37f84035f4c895581682
Jakub <jakubczupajlo@gmail.com>
2026-02-15 15:32:59 +0100

    Create a file every second 2

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ def main() -> None:
         now = datetime.now()
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
 
-        h = now.hour
-        m = now.minute
-        s = now.second
-        file_name = f"app-{h}_{m}_{s}.log"
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+        file_name = f"app-{hour}_{minute}_{second}.log"
 
         with open(file_name, "w") as file:
             file.write(timestamp)
