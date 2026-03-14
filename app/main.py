62c33591ced44e1e617c185f8927d39de78b0d2b
Dmytro Shulha <dimonchik0218@gmail.com>
2026-03-14 17:48:01 +0200

    task create-file-second done

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
-
+from time import sleep
 
 def main():
-    # write your code here
-    pass
+    while True:
+        now = datetime.now()
+        file_name = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        with open(file_name, "w") as f:
+            f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+        sleep(1)
+        print(now.strftime("%Y-%m-%d %H:%M:%S"), file_name)
 
 
 if __name__ == "__main__":
